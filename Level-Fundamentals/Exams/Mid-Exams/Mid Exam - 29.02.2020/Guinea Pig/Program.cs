c607107b4c5ada9667df5477a605c42760047e94
Dilyan-Iliev <dilqnn.ilievv@abv.bg>
2022-04-01 09:02:54 +0200

    Exam from 29.02.2020

---
+++ Level-Fundamentals/Exams/Mid-Exams/Mid Exam - 29.02.2020/Guinea Pig/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Guinea_Pig
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double foodQuantity = double.Parse(Console.ReadLine()) * 1000;
+            double hayQuantity = double.Parse(Console.ReadLine()) * 1000;
+            double coverQuantity = double.Parse(Console.ReadLine()) * 1000;
+            double guineaWeight = double.Parse(Console.ReadLine()) * 1000;
+
+            int daysToCover = 30;
+
+            //Every day Puppy eats 300 gr of food
+            //Every second day Merry first feeds the pet, then gives it a certain amount of hay equal to 5% of the rest of the food
+            //On every third day, Merry puts Puppy cover with a quantity of 1/3 of its weight.
+
+            for (int i = 1; i <= daysToCover; i++)
+            {//Calculate whether the quantity of food, hay, and cover, will be enough for a month.
+
+                foodQuantity -= 300;
+
+                if (foodQuantity <= 0 || hayQuantity <=0 || coverQuantity <= 0)
+                {
+                    Console.WriteLine("Merry must go to the pet store!");
+                    return ;
+                }
+
+                if (i % 2 == 0)
+                {
+                    double neededHay = foodQuantity * 0.05;
+                    hayQuantity = hayQuantity - neededHay;
+                }
+                if (i % 3 == 0)
+                {
+                    double neededCover = guineaWeight / 3;
+                    coverQuantity -= neededCover;
+                }
+            }
+            double foodLeftInKg = foodQuantity / 1000;
+            double hayLeftInKg = hayQuantity / 1000;
+            double coverLeftInKg = coverQuantity / 1000;
+            Console.WriteLine($"Everything is fine! Puppy is happy! Food: {foodLeftInKg:f2}, Hay: {hayLeftInKg:f2}, Cover: {coverLeftInKg:f2}.");
+        }
+    }
+}
---
+++ Level-Fundamentals/Exams/Mid-Exams/Mid Exam - 29.02.2020/Shopping List/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Shopping_List
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> products = Console.ReadLine()
+                .Split('!', StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+
+            string command;
+
+            while ((command = Console.ReadLine()) != "Go Shopping!")
+            {
+                string[] cmdArgs = command.Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+
+                products = ListCommands(products, cmdArgs);
+            }
+
+            Console.WriteLine(string.Join(", ",products));
+        }
+
+        static List<string> ListCommands (List<string> list, string[] cmdArgs)
+        {
+            string currentCommand = cmdArgs[0];
+
+            switch (currentCommand)
+            {
+                case "Urgent": list = UrgentCmd(list, cmdArgs); break;
+                case "Unnecessary": list = UnnecessaryCmd(list, cmdArgs); break;
+                case "Correct": list = CorrectCmd(list, cmdArgs); break;    
+                case "Rearrange": list = RearrangeCmd(list, cmdArgs); break;
+            }
+            return list;
+        }
+
+        static List<string> UrgentCmd (List<string> list, string[] cmdArgs)
+        {
+            //add the item at the start of the list.  If the item already exists, skip this command.
+            string itemToAdd = cmdArgs[1];
+
+            if (!list.Contains(itemToAdd))
+            {
+                list.Insert(0,itemToAdd);
+            }
+            return list;
+        }
+
+        static List<string> UnnecessaryCmd (List<string> list, string[] cmdArgs)
+        {
+            //remove the item with the given name, only if it exists in the list. Otherwise, skip this command.
+            string itemToRemove = cmdArgs[1];
+
+            if (list.Contains(itemToRemove))
+            {
+                list.Remove(itemToRemove);
+            }
+            return list;
+        }
+
+        static List<string> CorrectCmd (List<string> list, string[] cmdArgs)
+        {
+            // if the item with the given old name exists, change its name with the new one. Otherwise, skip this command
+            string oldItem = cmdArgs[1];
+            string newItem = cmdArgs[2];
+
+            int index = list.IndexOf(oldItem);
+            if (list.Contains(oldItem))
+            {
+                string currentItem = newItem;
+                //newItem = oldItem;
+                list.Insert(index, currentItem);
+                list.Remove(oldItem);
+            }
+            return list;
+        }
+
+        static List<string> RearrangeCmd (List<string> list, string[] cmdArgs)
+        {
+            //if the grocery exists in the list, remove it from its current position and add it at the end of the list. Otherwise, skip this command
+            string item = cmdArgs[1];
+
+            if (list.Contains(item))
+            {
+            list.Remove(item);
+            list.Add(item);
+            }
+
+            return list;
+        }
+    }
+}
