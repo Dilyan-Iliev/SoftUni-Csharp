b9ec888a24e49b951f1706bb1b3e62e3af7eeead
Dilyan-Iliev <dilqnn.ilievv@abv.bg>
2022-04-02 13:50:31 +0200

    Exam from 12.08.2020

---
+++ Level-Fundamentals/Exams/Mid-Exams/Mid Exam - 12.08.2020/Computer Store/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Computer_Store
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+            double totalPriceWithouTaxes = 0;
+            bool isSpecial = false;
+            while (true)
+            {
+                if (command == "special")
+                {
+                    isSpecial = true;
+                    break;
+                }
+                if (command == "regular")
+                {
+                    break;
+                }
+                double partPrice = double.Parse(command);
+
+                if (partPrice < 0)
+                {
+                    Console.WriteLine("Invalid price!");
+                    command = Console.ReadLine();
+                    continue;
+                }
+                totalPriceWithouTaxes += partPrice;
+
+                command = Console.ReadLine();
+            }
+
+            double tax = totalPriceWithouTaxes * 0.2;
+            double finalPrice = totalPriceWithouTaxes + tax;
+            if (finalPrice == 0)
+            {
+                Console.WriteLine("Invalid order!");
+                return;
+            }
+            else
+            {
+                Console.WriteLine("Congratulations you've just bought a new computer!");
+                Console.WriteLine($"Price without taxes: {totalPriceWithouTaxes:f2}$");
+                Console.WriteLine($"Taxes: {tax:f2}$");
+                Console.WriteLine("-----------");
+                if (isSpecial)
+                {
+                    finalPrice = finalPrice - (finalPrice * 0.1);
+                }
+                else
+                {
+                    finalPrice = finalPrice;
+                }
+
+                Console.WriteLine($"Total price: {finalPrice:f2}$");
+            }
+
+        }
+    }
+}
---
+++ Level-Fundamentals/Exams/Mid-Exams/Mid Exam - 12.08.2020/Memory Game/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Memory_Game
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            List<string> values = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+
+            string command = Console.ReadLine();
+
+            int movesCounter = 0;
+            while (true)
+            {
+
+                if (command == "end" && values.Count > 0)
+                {
+                    Console.WriteLine("Sorry you lose :(");
+                    Console.WriteLine(String.Join(" ", values));
+                    break;
+                }
+                if (command == "end")
+                {
+                    break;
+                }
+
+                string[] cmdArgs = command.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                int firstIndex = int.Parse(cmdArgs[0]);
+                int secondIndex = int.Parse(cmdArgs[1]);
+
+                if (firstIndex >= 0 && firstIndex < values.Count
+                    && secondIndex >= 0 && secondIndex < values.Count)
+                {
+                    if (values[firstIndex] == values[secondIndex])
+                    {
+                        movesCounter++;
+                        string element1 = values.ElementAt(int.Parse(cmdArgs[0]));
+                        Console.WriteLine($"Congrats! You have found matching elements - {values[firstIndex]}!");
+                        values.RemoveAll(x => x.Equals(element1));
+                    }
+                    else if (values[firstIndex] != values[secondIndex])
+                    {
+                        Console.WriteLine("Try again!");
+                        movesCounter++;
+                    }
+
+                    if (values.Count == 0)
+                    {
+                        Console.WriteLine($"You have won in {movesCounter} turns!");
+                        break;
+                    }
+                }
+                else
+                {
+                    movesCounter++;
+                    int placeToInsert = values.Count / 2;
+                    string elementToInsert = $"-{movesCounter}a";
+
+
+                    if (firstIndex == secondIndex)
+                    {
+                        Console.WriteLine("Invalid input! Adding additional elements to the board");
+                    }
+
+                    if (firstIndex < 0 || firstIndex >= values.Count)
+                    {
+                        Console.WriteLine("Invalid input! Adding additional elements to the board");
+                    }
+
+                    else if (secondIndex < 0 || secondIndex >= values.Count)
+                    {
+                        Console.WriteLine("Invalid input! Adding additional elements to the board");
+                    }
+                    for (int i = 1; i <= 2; i++)
+                    {
+                        values.Insert(placeToInsert, elementToInsert);
+                    }
+
+                }
+                command = Console.ReadLine();
+            }
+
+
+        }
+    }
+}
