9750af64600da73b3c19df3197ae9e209f47fc0e
Dilyan-Iliev <dilqnn.ilievv@abv.bg>
2022-04-04 15:31:39 +0200

    Add tasks from first lection C#-Advanced

---
+++ Level-Advanced/Labs/Stacks and Queues - Lab/P.04-Matching Brackets/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P._04_Matching_Brackets
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            Stack<int> stack = new Stack<int>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                char currentCh = input[i];
+
+                if (currentCh == '(')
+                {
+                    stack.Push(i);
+                }
+                else if (currentCh == ')')
+                {
+                    int startIndex = stack.Pop();
+                    string substring = input.Substring(startIndex, i - startIndex + 1);
+                    Console.WriteLine(substring);
+                }
+            }
+        }
+    }
+}
