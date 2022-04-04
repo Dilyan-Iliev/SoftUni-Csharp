ff1757f775107f5cbfe836ca87e0a8b2ec749a4f
Dilyan-Iliev <dilqnn.ilievv@abv.bg>
2022-04-04 15:30:31 +0200

    Add tasks from first lection C#-Fundamentals

---
+++ Level-Advanced/Labs/Stacks and Queues - Lab/P.03-SimpleCalculator/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P._03_SimpleCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(' ');
+            Stack<string> stack = new Stack<string>(input.Reverse());
+
+            while (stack.Count != 1)
+            {
+                int num1 = int.Parse(stack.Pop()); //вземам последния елемент в stack-a , който ще е число
+                char @operator = char.Parse(stack.Pop()); //вземам последния елемент в stack-a, който е оператор
+                int num2 = int.Parse(stack.Pop()); //вземам последния елемент в stack-a, който е число
+
+                if (@operator == '+')
+                {
+                    stack.Push((num1 + num2).ToString());
+                }
+                else if (@operator == '-')
+                {
+                    stack.Push((num1-num2).ToString());
+                }
+            }
+            Console.WriteLine(stack.Peek());
+        }
+    }
+}
