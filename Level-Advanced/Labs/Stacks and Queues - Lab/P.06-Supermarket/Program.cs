f6c53c99498377a388a19a7dd61704c443f71bfa
Dilyan-Iliev <dilqnn.ilievv@abv.bg>
2022-04-05 09:12:37 +0200

    Add tasks from first lection C#-Advanced

---
+++ Level-Advanced/Labs/Stacks and Queues - Lab/P.06-Supermarket/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace P._06_Supermarket
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+
+            Queue<string> queue = new Queue<string>();
+            queue.Enqueue(name);
+
+            while (true)
+            {
+                name = Console.ReadLine();
+                if (name == "End")
+                {
+                    break;
+                }
+                if (name == "Paid")
+                {
+                    Console.WriteLine(string.Join(Environment.NewLine, queue));
+                    queue.Clear();
+                }
+                else
+                {
+                    queue.Enqueue(name);
+                }
+            }
+            Console.WriteLine($"{queue.Count} people remaining.");
+        }
+    }
+}
