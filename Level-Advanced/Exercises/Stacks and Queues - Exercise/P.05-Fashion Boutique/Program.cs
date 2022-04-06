b13cd37f122e5bb38150068b3780631265a89e18
Dilyan-Iliev <dilqnn.ilievv@abv.bg>
2022-04-06 09:48:59 +0200

    Add tasks from first exercise C#-Advanced

---
+++ Level-Advanced/Exercises/Stacks and Queues - Exercise/P.05-Fashion Boutique/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace P._05_Fashion_Boutique
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] clothesInABox = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int singleRackCapacity = int.Parse(Console.ReadLine());
+
+            Stack<int> stack = new Stack<int>(clothesInABox);
+
+            int racksCounter = 1;
+            int clothesSum = 0;
+
+            while (stack.Count > 0)
+            {
+                int currentState = stack.Pop(); //броя на дрехите на съответната итерация
+                bool isFull = clothesSum + currentState <= singleRackCapacity;
+
+                if (isFull)
+                {
+                    clothesSum += currentState;
+                }
+                else
+                {
+                    clothesSum = currentState;
+                    racksCounter++;
+                }
+            }
+            Console.WriteLine(racksCounter);
+        }
+    }
+}
