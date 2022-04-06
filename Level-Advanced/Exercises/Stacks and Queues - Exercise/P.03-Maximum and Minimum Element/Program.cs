2294977cf1235f5935fd6f1257830688c264af6a
Dilyan-Iliev <dilqnn.ilievv@abv.bg>
2022-04-06 09:47:05 +0200

    Add task from first exercise C#-Advanced

---
+++ Level-Advanced/Exercises/Stacks and Queues - Exercise/P.03-Maximum and Minimum Element/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P._03_Maximum_and_Minimum_Element
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> stack = new Stack<int>();
+
+            int iterations = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= iterations; i++)
+            {
+                int[] arr = Console.ReadLine()
+                    .Split()
+                    .Select(int.Parse)
+                    .ToArray();
+
+                int cmd = arr[0];
+
+                if (cmd == 1)
+                {
+                    int numberToPush = arr[1];
+                    stack.Push(numberToPush);
+                }
+                else if (cmd == 2)
+                {
+                    if (stack.Count > 0)
+                    {
+                        stack.Pop();
+                    }
+                }
+                else if (cmd == 3)
+                {
+                    if (stack.Count == 0)
+                    {
+                        continue;
+                    }
+                    Console.WriteLine(stack.Max());
+                }
+                else if (cmd == 4)
+                {
+                    if (stack.Count == 0)
+                    {
+                        continue;
+                    }
+                    Console.WriteLine(stack.Min());
+                }
+            }
+
+            if (stack.Count > 0)
+            {
+                Console.WriteLine(string.Join(", ", stack));
+            }
+        }
+    }
+}
