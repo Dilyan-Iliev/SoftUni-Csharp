576c93f3b76ef4d6c02fb536b94b6fa50c1c4271
Dilyan-Iliev <dilqnn.ilievv@abv.bg>
2022-04-07 10:07:14 +0200

    Add tasks from first exercise C#-Advanced

---
+++ Level-Advanced/Exercises/Stacks and Queues - Exercise/P.08-Balanced Parenthesis/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P._08_Balanced_Parenthesis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            Stack<char> stack = new Stack<char>();
+            bool isValid = true;
+
+            foreach(var symbol in input)
+            {
+                if (symbol == '(' || symbol == '[' || symbol == '{')
+                {
+                    stack.Push(symbol);
+                }
+                else
+                {
+                    if (stack.Count == 0)
+                    {
+                        isValid = false;
+                        break;
+                    }
+
+                    if (symbol == '}' && stack.Peek()  == '{')
+                    {
+                        stack.Pop();
+                    }
+                    else if (symbol == ']' && stack.Peek() == '[')
+                    {
+                        stack.Pop();
+                    }
+                    else if (symbol == ')' && stack.Peek() == '(')
+                    {
+                        stack.Pop();
+                    }
+                    else
+                    {
+                        isValid = false;
+                    }
+                }
+            }
+
+            if (isValid)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+        }
+    }
+}
