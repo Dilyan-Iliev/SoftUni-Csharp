079ed4a18e012750fdc0e6abe90603edb15812f9
Dilyan-Iliev <dilqnn.ilievv@abv.bg>
2022-04-07 10:08:00 +0200

    Add tasks from first exercise C#-Advanced

---
+++ Level-Advanced/Exercises/Stacks and Queues - Exercise/P.09-Simple Text Editor/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace P._09_Simple_Text_Editor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<string> stack = new Stack<string>();
+
+            int n = int.Parse(Console.ReadLine());
+
+            string text = string.Empty;
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] command = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                string currentCmd = command[0];
+
+                if (currentCmd == "1")
+                {
+                    stack.Push(text);
+                    string textToAdd = command[1];
+                    text += textToAdd;
+                }
+                else if (currentCmd == "2")
+                {
+                    int numberOfElementsToErase = int.Parse(command[1]);
+                    stack.Push(text);
+
+                    for (int j = 0; j < numberOfElementsToErase; j++)
+                    {
+                        text = text.Remove(text.Length - 1);
+                    }
+
+                }
+                else if (currentCmd == "3")
+                {
+                    int elementToPrint = int.Parse(command[1]);
+
+                    Console.WriteLine(text[elementToPrint - 1]);
+                }
+                else if (currentCmd == "4")
+                {
+                    text = stack.Pop();
+                }
+            }
+        }
+    }
+}
