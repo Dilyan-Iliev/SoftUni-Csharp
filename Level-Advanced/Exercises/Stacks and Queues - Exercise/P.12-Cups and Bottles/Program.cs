a6d8f29a304732d47432c5dc9940108fa9529a3f
Dilyan-Iliev <dilqnn.ilievv@abv.bg>
2022-04-08 09:15:34 +0200

    Add tasks from first exercise C#-Advanced

---
+++ Level-Advanced/Exercises/Stacks and Queues - Exercise/P.12-Cups and Bottles/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace P._12_Cups_and_Bottles
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] cupsCapacity = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int[] bottlesWithWater = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Stack<int> bottlesStack = new Stack<int>(bottlesWithWater);
+            Queue<int> cupsQueue = new Queue<int>(cupsCapacity);
+
+            int wastedWater = 0;
+            bool isNewOne = true;
+            int currentCup = 0;
+
+            while (bottlesStack.Any() && cupsQueue.Any())
+            {
+                if (isNewOne)
+                {
+                    currentCup = cupsQueue.Peek();
+                    isNewOne = false;
+                }
+                int currentBottle = bottlesStack.Pop();
+
+                if (currentCup - currentBottle <= 0)
+                {
+                    int left = currentCup - currentBottle;
+                    wastedWater += left;
+                    cupsQueue.Dequeue();
+                    isNewOne = true;
+                }
+                else
+                {
+                    currentCup -= currentBottle;
+                }
+            }
+
+            if (cupsQueue.Any())
+            {
+                Console.WriteLine($"Cups: {string.Join(' ', cupsQueue)}");
+            }
+            else if (bottlesStack.Any())
+            {
+                Console.WriteLine($"Bottles: {string.Join(' ', bottlesStack)}");
+            }
+            Console.WriteLine($"Wasted litters of water: {Math.Abs(wastedWater)}");
+        }
+    }
+}
