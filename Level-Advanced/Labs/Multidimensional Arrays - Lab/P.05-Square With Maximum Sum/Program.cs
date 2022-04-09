de537f88e8d572d1697923ddd31e6aa367d71a85
Dilyan-Iliev <dilqnn.ilievv@abv.bg>
2022-04-09 13:51:49 +0200

    Add tasks from second lection C#-Advanced

---
+++ Level-Advanced/Labs/Multidimensional Arrays - Lab/P.05-Square With Maximum Sum/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Linq;
+
+namespace P._05_Square_With_Maximum_Sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] matrixDimensions = Console.ReadLine()
+                .Split(",")
+                .Select(int.Parse)
+                .ToArray();
+
+            int rows = matrixDimensions[0];
+            int cols = matrixDimensions[1];
+
+            int[,] matrix = new int[rows, cols];
+
+            for (int row = 0; row < rows; row++)
+            {
+                int[] matrixData = Console.ReadLine()
+                    .Split(", ")
+                    .Select(int.Parse)
+                    .ToArray();
+
+                for (int col = 0; col < cols; col++)
+                {
+                    matrix[row, col] = matrixData[col];
+                }
+            }
+
+            long maxSum = long.MinValue;
+            int maxRow = 0;
+            int maxCol = 0;
+
+            for (int row = 0; row < matrix.GetLength(0) - 1; row++)
+            {
+                int currentSum = 0;
+                for (int col = 0; col < matrix.GetLength(1) - 1; col++)
+                {
+                    currentSum = matrix[row, col] + matrix[row, col + 1]
+                        + matrix[row + 1, col] + matrix[row + 1, col + 1];
+
+                    if (currentSum > maxSum)
+                    {
+                        maxSum = currentSum;
+                        maxRow = row;
+                        maxCol = col;
+                    }
+                }
+            }
+
+            for (int row = maxRow; row < maxRow + 2; row++)
+            {
+                for (int col = maxCol; col < maxCol + 2; col++)
+                {
+                    Console.Write(matrix[row, col] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine(maxSum);
+        }
+    }
+}
