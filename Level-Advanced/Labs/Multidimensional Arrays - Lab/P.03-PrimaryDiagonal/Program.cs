970865b5c5778be9db8214f4e02cbe40384a8ae4
Dilyan-Iliev <dilqnn.ilievv@abv.bg>
2022-04-09 13:50:28 +0200

    Add tasks from second lection C#-Advanced

---
+++ Level-Advanced/Labs/Multidimensional Arrays - Lab/P.03-PrimaryDiagonal/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+
+namespace P._03_PrimaryDiagonal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int matrixRowsAndCols = int.Parse(Console.ReadLine());
+
+            int[,] table = new int[matrixRowsAndCols, matrixRowsAndCols];
+
+            for (int row = 0; row < matrixRowsAndCols; row++)
+            {
+                int[] matrixData = Console.ReadLine()
+                    .Split()
+                    .Select(int.Parse).ToArray();
+
+                for (int col = 0; col < matrixRowsAndCols; col++)
+                {
+                    table[row, col] = matrixData[col];
+                }
+            }
+
+            int matrixDiagonalSum = 0;
+
+            for (int row = 0; row < table.GetLength(0); row++)
+            {
+                
+                for (int col = 0; col < table.GetLength(1); col++)
+                {
+                    if (row == col)
+                    {
+                        matrixDiagonalSum += table[row, col]; 
+                    //винаги ми трябва елемент, при който номера на реда е равен на номера на колоната
+                    }
+                }
+            }
+            Console.WriteLine(matrixDiagonalSum);
+        }
+    }
+}
