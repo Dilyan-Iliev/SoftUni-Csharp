13fc81523a8e935e6f2afd70b0dc8e79ae4fc032
Dilyan-Iliev <dilqnn.ilievv@abv.bg>
2022-04-09 13:49:39 +0200

    Add tasks from second lection C#-Advanced

---
+++ Level-Advanced/Labs/Multidimensional Arrays - Lab/P.02-SumMatrixColumns/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+
+namespace P._02_SumMatrixColumns
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] matrixDimensions = Console.ReadLine()
+                .Split(", ")
+                .Select(int.Parse)
+                .ToArray();
+
+            int rows = matrixDimensions[0];
+            int cols = matrixDimensions[1];
+
+            int[,] table = new int[rows, cols];
+
+            for (int row = 0; row < rows; row++)
+            {
+                int[] tableData = Console.ReadLine()
+                    .Split()
+                    .Select(int.Parse)
+                    .ToArray();
+
+                for (int col = 0; col < cols; col++)
+                {
+                    table[row, col] = tableData[col];
+                }
+            }
+
+
+            for (int col = 0; col < table.GetLength(1); col++)
+            {
+                int tableColsSum = 0;
+                for (int row = 0; row < table.GetLength(0); row++)
+                {
+                    int currentElement = table[row, col];
+                    tableColsSum += currentElement;
+                }
+                Console.WriteLine(tableColsSum);
+            }
+        }
+    }
+}
