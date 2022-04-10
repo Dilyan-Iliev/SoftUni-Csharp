752ef47fc01071e24fc744385c7affe21f3440a9
Dilyan-Iliev <dilqnn.ilievv@abv.bg>
2022-04-10 16:51:27 +0200

    Add tasks from second exercise C#-Advanced

---
+++ Level-Advanced/Exercises/Multidimensional Arrays - Exercise/P.01-Diagonal Difference/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+
+namespace P._01_Diagonal_Difference
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[,] matrix = new int[n,n];
+
+            for (int row = 0; row < n; row++)
+            {
+                int[] matrixData = Console.ReadLine()
+                    .Split()
+                    .Select(int.Parse)
+                    .ToArray();
+
+                for (int col = 0; col < n; col++)
+                {
+                    matrix[row,col] = matrixData[col];
+                }
+            }
+
+            int firstSum = 0;
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    if (row == col)
+                    {
+                        firstSum += matrix[row, col];
+                    }
+                }
+            }
+
+            int secondSum = 0;
+            int counter = matrix.GetLength(1) - 1; 
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                secondSum += matrix[row, counter];
+                counter--; //за да вземаме 1во последния елемент на реда, послед предпоследния и после предпредпоследния
+            }
+            int difference = Math.Abs(firstSum - secondSum);
+            Console.WriteLine(difference);
+        }
+    }
+}
