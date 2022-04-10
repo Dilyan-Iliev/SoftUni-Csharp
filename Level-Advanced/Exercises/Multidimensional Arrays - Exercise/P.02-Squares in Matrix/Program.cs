00eacd1abf9a77d6cc85da929a816199322f0375
Dilyan-Iliev <dilqnn.ilievv@abv.bg>
2022-04-10 16:52:26 +0200

    Add tasks from second exercise C#-Advanced

---
+++ Level-Advanced/Exercises/Multidimensional Arrays - Exercise/P.02-Squares in Matrix/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+
+namespace P._02_Squares_in_Matrix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] matrixDimensions = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int rows = matrixDimensions[0];
+            int cols = matrixDimensions[1];
+
+            string[,] matrix = new string[rows, cols];
+
+            for (int row = 0; row < rows; row++)
+            {
+                string[] arr = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                for (int col = 0; col < cols; col++)
+                {
+                    matrix[row, col] = arr[col];
+                }
+            }
+
+            int counter = 0;
+
+            for (int row = 0; row < matrix.GetLength(0) - 1; row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1) - 1; col++)
+                {
+                    if (matrix[row, col] == matrix[row, col + 1] 
+                        && matrix[row, col] == matrix[row+1,col]
+                        && matrix[row, col] == matrix[row+1,col+1])
+                    {
+                        counter++;
+                    }
+                }
+            }
+            Console.WriteLine(counter);
+        }
+    }
+}
