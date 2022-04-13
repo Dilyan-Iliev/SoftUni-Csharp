bf4c46c29481ffd036fef5445a92a00017312356
Dilyan-Iliev <dilqnn.ilievv@abv.bg>
2022-04-13 10:10:32 +0200

    Add tasks from third lection C#-Advanced

---
+++ Level-Advanced/Labs/Sets and Dictionaries Advanced - Lab/P.03-Largest 3 numbers/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace P._03_Largest_3_numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split(' ',StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .OrderByDescending(x => x)
+                .Take(3)
+                .ToArray();
+
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
