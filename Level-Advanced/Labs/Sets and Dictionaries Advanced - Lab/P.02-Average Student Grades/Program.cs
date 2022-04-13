913d0c1391448e75850d1b2fe03fc0f3881938ba
Dilyan-Iliev <dilqnn.ilievv@abv.bg>
2022-04-13 10:09:40 +0200

    Add tasks from third lection C#-Advanced

---
+++ Level-Advanced/Labs/Sets and Dictionaries Advanced - Lab/P.02-Average Student Grades/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace P._02_Average_Student_Grades
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfStudents = int.Parse(Console.ReadLine());
+
+            Dictionary<string, List<decimal>> studentGrades = new Dictionary<string, List<decimal>>();
+
+            for (int student = 1; student <= numberOfStudents; student++)
+            {
+                string[] info = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                string studentName = info[0];
+                decimal studentGrade = decimal.Parse(info[1]);
+
+                if (!studentGrades.ContainsKey(studentName))
+                {
+                    studentGrades.Add(studentName, new List<decimal>());
+                    studentGrades[studentName].Add(studentGrade);
+                }
+                else
+                {
+                    studentGrades[studentName].Add(studentGrade);
+                }
+            }
+
+            foreach (var kvp in studentGrades)
+            {
+                string student = kvp.Key;
+                List<decimal> grades = kvp.Value;
+                decimal average = grades.Average();
+
+                Console.Write($"{student} -> ");
+                foreach (var grade in grades)
+                {
+                    Console.Write($"{grade:f2} ");
+                }
+                Console.WriteLine($"(avg: {average:f2})");
+
+                //Console.WriteLine($"{student} -> {string.Join(" ", grades)} (avg: {average:f2})");
+            }
+        }
+    }
+}
