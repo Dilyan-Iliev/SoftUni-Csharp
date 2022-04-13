86cc9d98afe418edb6b7869b3b20f761675bd897
Dilyan-Iliev <dilqnn.ilievv@abv.bg>
2022-04-13 10:13:52 +0200

    Add tasks from C#-Advanced

---
+++ Level-Advanced/Labs/Sets and Dictionaries Advanced - Lab/P.07-Parking Lot/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P._07_Parking_Lot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> set = new HashSet<string>();
+
+            string input;
+            while ((input = Console.ReadLine()) != "END")
+            {
+                string[] inputArgs = input
+                    .Split(", ", StringSplitOptions.RemoveEmptyEntries);
+
+                string direction = inputArgs[0];
+                string carNumber = inputArgs[1];
+
+                if (direction == "IN")
+                {
+                    set.Add(carNumber);
+                }
+                else if (direction == "OUT")
+                {
+                    set.Remove(carNumber);
+                }
+            }
+
+            if (set.Count == 0)
+            {
+                Console.WriteLine("Parking Lot is Empty");
+            }
+            else
+            {
+                foreach (var carNumber in set)
+                {
+                    Console.WriteLine(carNumber);
+                }
+            }
+        }
+    }
+}
