ab5b6fcb18909aaaae03841c9d1f6272ac129373
Dilyan-Iliev <dilqnn.ilievv@abv.bg>
2022-04-13 10:08:46 +0200

    Add tasks from third lection C#-Advanced

---
+++ Level-Advanced/Labs/Sets and Dictionaries Advanced - Lab/P.01-Count Same Values in Array/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace P._01_Count_Same_Values_in_Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] numbers = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                
+
+            Dictionary<string, int> numbersDictionary = new Dictionary<string, int>();
+
+            foreach (var number in numbers)
+            {
+                if (!numbersDictionary.ContainsKey(number))
+                {
+                    numbersDictionary[number] = 0;
+                }
+                numbersDictionary[number]++;    
+            }
+
+            foreach (var kvp in numbersDictionary)
+            {
+                Console.WriteLine($"{kvp.Key} - {kvp.Value} times");
+            }
+        }
+    }
+}
