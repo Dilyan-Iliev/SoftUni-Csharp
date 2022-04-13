fc274e9641a4d6653f1fae4744786fb6e09e552f
Dilyan-Iliev <dilqnn.ilievv@abv.bg>
2022-04-13 10:12:18 +0200

    Add tasks from third lection C#-Advanced

---
+++ Level-Advanced/Labs/Sets and Dictionaries Advanced - Lab/P.05-Cities by Continent and Country/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace P._05_Cities_by_Continent_and_Country
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            var dictionary = new Dictionary<string, Dictionary<string, List<string>>>();
+
+            for (int i = 1; i <= n; i++)
+            {
+                string[] info = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                string continent = info[0];
+                string country = info[1];
+                string city = info[2];
+
+                if (!dictionary.ContainsKey(continent))
+                {
+                    dictionary[continent] = new Dictionary<string, List<string>>();
+                }
+
+                if (!dictionary[continent].ContainsKey(country))
+                {
+                    dictionary[continent][country] = new List<string>();
+                    dictionary[continent][country].Add(city);
+                }
+                else
+                {
+                    dictionary[continent][country].Add(city);
+                }
+            }
+            foreach (var continent in dictionary)
+            {
+                Console.WriteLine($"{continent.Key}:");
+
+                foreach (var country in continent.Value)
+                {
+                    Console.WriteLine($"{country.Key} -> {string.Join(", ", country.Value)}");
+                }
+            }
+        }
+    }
+}
