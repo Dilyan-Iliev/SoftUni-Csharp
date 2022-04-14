d17fdd163d2ccb1b573af5137abaffe2c6436003
Dilyan-Iliev <dilqnn.ilievv@abv.bg>
2022-04-14 09:51:18 +0200

    Add tasks from third exercise C#-Advanced

---
+++ Level-Advanced/Exercises/Sets and Dictionaries Advanced - Exercise/P.01-Unique Usernames/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P._01_Unique_Usernames
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfLines = int.Parse(Console.ReadLine());
+
+            HashSet<string> names = new HashSet<string>();
+
+            for (int i = 1; i <= numberOfLines; i++)
+            {
+                string name = Console.ReadLine();
+                names.Add(name);
+            }
+            Console.WriteLine(string.Join(Environment.NewLine, names));
+        }
+    }
+}
