5e505641d82a846df1d518cfb8974265b5d406a0
Dilyan-Iliev <dilqnn.ilievv@abv.bg>
2022-04-14 09:53:47 +0200

    Add tasks from third exercise C#-Advanced

---
+++ Level-Advanced/Exercises/Sets and Dictionaries Advanced - Exercise/P.03-Periodic Table/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace P._03_Periodic_Table
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            SortedSet<string> chemicalsCompounds = new SortedSet<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] chemicals = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                for (int k = 0; k < chemicals.Length; k++)
+                {
+                    chemicalsCompounds.Add(chemicals[k]);
+                }
+            }
+            Console.WriteLine(string.Join(' ', chemicalsCompounds));
+        }
+    }
+}
