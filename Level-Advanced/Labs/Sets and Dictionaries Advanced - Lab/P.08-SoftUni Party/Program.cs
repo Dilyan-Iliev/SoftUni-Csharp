a668c114b9ade99df9094bec108f50213554dc21
Dilyan-Iliev <dilqnn.ilievv@abv.bg>
2022-04-14 09:48:55 +0200

    Add tasks from third lection C#-Advanced

---
+++ Level-Advanced/Labs/Sets and Dictionaries Advanced - Lab/P.08-SoftUni Party/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P._08_SoftUni_Party
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> regularGuests = new HashSet<string>();
+            HashSet<string> VIPGuests = new HashSet<string>();
+
+            string input;
+            while ((input = Console.ReadLine()) != "PARTY")
+            {
+                if (char.IsDigit(input[0]))
+                {
+                    VIPGuests.Add(input);
+                }
+                else
+                {
+                    regularGuests.Add(input);
+                }
+            }
+
+            string removeInput;
+            while ((removeInput = Console.ReadLine()) != "END")
+            {
+                if (VIPGuests.Contains(removeInput))
+                {
+                    VIPGuests.Remove(removeInput);
+                }
+                else if (regularGuests.Contains(removeInput))
+                {
+                    regularGuests.Remove(removeInput);
+                }
+            }
+            Console.WriteLine(regularGuests.Count + VIPGuests.Count);
+            foreach (var guest in VIPGuests)
+            {
+                Console.WriteLine(guest);
+            }
+            foreach (var guest in regularGuests)
+            {
+                Console.WriteLine(guest);
+            }
+        }
+    }
+}
