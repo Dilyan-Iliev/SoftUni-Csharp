0ed4cf554be0477ba709247ad97ef66314a3ac6c
Dilyan-Iliev <dilqnn.ilievv@abv.bg>
2022-04-15 12:57:40 +0200

    Add tasks from third exercise C#-Advanced

---
+++ Level-Advanced/Exercises/Sets and Dictionaries Advanced - Exercise/P.05-Count Symbols/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace P._05_Count_Symbols
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            SortedDictionary<char, int> sortedDictionary = new SortedDictionary<char, int>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (!sortedDictionary.ContainsKey(input[i]))
+                {
+                    sortedDictionary[input[i]] = 0;
+                }
+                sortedDictionary[input[i]]++;
+            }
+
+            foreach (var kvp in sortedDictionary)
+            {
+                Console.WriteLine($"{kvp.Key}: {kvp.Value} time/s");
+            }
+        }
+    }
+}
