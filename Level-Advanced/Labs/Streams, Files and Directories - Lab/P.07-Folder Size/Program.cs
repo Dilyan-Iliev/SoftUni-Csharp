30020d796943e82d59a5f8784197f34581bd1414
Dilyan-Iliev <dilqnn.ilievv@abv.bg>
2022-04-18 15:34:10 +0200

    Add tasks from fourth lection C#-Advanced

---
+++ Level-Advanced/Labs/Streams, Files and Directories - Lab/P.07-Folder Size/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+
+namespace P._07_Folder_Size
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double size = GetLength("D:/Test");
+
+            using StreamWriter writer = new StreamWriter("../../../Output.txt") ;
+            writer.WriteLine($"{size} KB");
+
+
+        }
+        static double GetLength(string directory)
+        {
+            double size = 0;
+            string[] files = Directory.GetFiles(directory);
+            string[] subDirectories = Directory.GetDirectories(directory);
+
+            foreach (string file in files)
+            {
+                FileInfo fileInfo = new FileInfo(file);
+                size += fileInfo.Length;
+            }
+            foreach (string subDirectory in subDirectories)
+            {
+                size += GetLength(subDirectory);
+            }
+
+
+            return size * 0.000001;
+        }
+
+    }
+}
