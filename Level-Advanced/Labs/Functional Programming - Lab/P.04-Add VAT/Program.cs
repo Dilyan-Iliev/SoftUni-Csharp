a78d4cc389650d011b19c0cfd217faaca208834b
Dilyan-Iliev <dilqnn.ilievv@abv.bg>
2022-04-20 16:10:22 +0200

    Add tasks from fifth lection C#-Advanced

---
+++ Level-Advanced/Labs/Functional Programming - Lab/P.04-Add VAT/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace P._04_Add_VAT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<string, double> parsingFunction = x => double.Parse(x);
+            Func<double, double> addPercantageFunction = x => x + x * 0.2;
+
+            var numbers = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(parsingFunction)
+                .Select(addPercantageFunction);
+                //.ToArray();
+
+            foreach (var number in numbers)
+            {
+                Console.WriteLine($"{number:F2}");
+            }
+        }
+    }
+}
