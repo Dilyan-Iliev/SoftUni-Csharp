c88b621aa648dfc126e79ae12cd52cc9aab04633
Dilyan-Iliev <dilqnn.ilievv@abv.bg>
2022-04-20 16:09:32 +0200

    Add tasks from fifth lection C#-Advanced

---
+++ Level-Advanced/Labs/Functional Programming - Lab/P.03-Count Uppercase Words/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace P._03_Count_Uppercase_Words
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] text = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            Func<string, bool> predicate = x => char.IsUpper(x[0]);
+
+            foreach (var word in text)
+            {
+                if (predicate(word))
+                {
+                    Console.WriteLine(word);
+                }
+            }
+        }
+    }
+}
