da1fbda23ea2cf3ba24d0c9043e8ed8234d4d02f
Dilyan-Iliev <dilqnn.ilievv@abv.bg>
2022-04-21 16:48:39 +0200

    Add tasks from fifth exercise C#-Advanced

---
+++ Level-Advanced/Exercises/Functional Programming - Exercise/P.01-Action Point/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace P._01_Action_Point
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            
+            Action<string[]> action = PrintResultOnNewLines; //присвоявам метода на абстрактен action
+            action(names); //извиквам метода с names - същото ще е ако кажа PrintResultOnNewLines(names)
+        }
+
+        static void PrintResultOnNewLines(string[] names)
+        {
+            foreach (var name in names)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
