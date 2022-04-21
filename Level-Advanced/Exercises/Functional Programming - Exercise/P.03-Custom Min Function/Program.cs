5b536d8661abd1b10e4814d610d4dc4cc6132846
Dilyan-Iliev <dilqnn.ilievv@abv.bg>
2022-04-21 16:49:57 +0200

    Add tasks from fifth exercise C#-Advanced

---
+++ Level-Advanced/Exercises/Functional Programming - Exercise/P.03-Custom Min Function/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace P._03_Custom_Min_Function
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<IEnumerable<int>, int> func = FindSmallestNumber;
+            var numbers = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse);
+
+            int minNumber = func(numbers);
+            Console.WriteLine(minNumber);
+        }
+
+        static int FindSmallestNumber(IEnumerable<int> numbers)
+        //IEnumerable makes it universal for all collections
+        {
+            int number = int.MaxValue;
+
+            foreach (var item in numbers)
+            {
+                if (item < number)
+                {
+                    number = item;
+                }
+            }
+            return number;
+        }
+    }
+}
