ea1f1101c2d92815bf7cb95b8b2b700a26aba3c1
Dilyan-Iliev <dilqnn.ilievv@abv.bg>
2022-04-21 16:51:06 +0200

    Add tasks from fifth exercise C#-Advanced

---
+++ Level-Advanced/Exercises/Functional Programming - Exercise/P.05-Applied Arithmetics/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace P._05_Applied_Arithmetics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+
+            string command = Console.ReadLine();
+            Action<string> operations = x =>
+            {//action which will aply when it is invoked (below in the while loop)
+                switch (x)
+                {
+                    case "add": numbers = numbers.Select(x => x + 1).ToArray(); break;
+                    case "multiply": numbers = numbers.Select(x => x * 2).ToArray(); break;
+                    case "subtract": numbers = numbers.Select(x => x - 1).ToArray(); break;
+                    case "print": Console.WriteLine(string.Join(" ", numbers)); break;
+                }
+            };
+
+            while (command  != "end")
+            {
+                operations(command);
+
+                command = Console.ReadLine();
+            }
+        }
+    }
+}
