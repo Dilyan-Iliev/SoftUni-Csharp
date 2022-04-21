ea4df78448da6702bdce9a448e38e84bc05708cf
Dilyan-Iliev <dilqnn.ilievv@abv.bg>
2022-04-21 16:49:19 +0200

    Add tasks from fifth exercise C#-Advanced

---
+++ Level-Advanced/Exercises/Functional Programming - Exercise/P.02-Knights of Honor/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace P._02_Knights_of_Honor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            Action<string[]> action = AppendElement;
+            action(names);
+        }
+
+        static void AppendElement(string[] names)
+        {
+            foreach (var name in names)
+            {
+                Console.WriteLine($"Sir {name}");
+            }
+        }
+    }
+}
