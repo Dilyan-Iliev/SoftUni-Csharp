83f624f827bb351fb67109be685b8e16f183482f
Dilyan-Iliev <dilqnn.ilievv@abv.bg>
2022-04-21 16:50:32 +0200

    Add tasks from fifth exercise C#-Advanced

---
+++ Level-Advanced/Exercises/Functional Programming - Exercise/P.04-Find Evens or Odds/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P._04_Find_Evens_or_Odds
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            string filterWord = Console.ReadLine();
+
+            int lowerBound = numbers[0];
+            int upperBound = numbers[1];
+
+            List<int> newNumbers = new List<int>();
+            for (int i = lowerBound; i <= upperBound; i++)
+            {
+                //fill new collection with numbers in the given range
+                newNumbers.Add(i);
+            }
+
+            if (filterWord == "odd")
+            {
+                IEnumerable<int> filteredCollection = Filter(newNumbers, x => x % 2 != 0);
+                Console.WriteLine(string.Join(" ", filteredCollection));
+            }
+            else if (filterWord == "even")
+            {
+                IEnumerable<int> filteredCollection = Filter(newNumbers, x => x % 2 == 0);
+                Console.WriteLine(string.Join(" ", filteredCollection));
+            }
+
+            //if (filterWord == "odd")
+            //{
+            //    var filteredNumbers = newNumbers.Where(x => x % 2 != 0);
+            //    Console.WriteLine(string.Join(" ", filteredNumbers));
+            //}
+            //else if (filterWord == "even")
+            //{
+            //    var filteredNumbers = newNumbers.Where(x => x % 2 == 0);
+            //    Console.WriteLine(string.Join(" ", filteredNumbers));
+            //}
+        }
+
+        static IEnumerable<int> Filter(IEnumerable<int> collection, Func<int, bool> filter)
+        {
+            List<int> result = new List<int>();
+            foreach (var item in collection)
+            {
+                if (filter(item))
+                {
+                    result.Add(item);
+                }
+            }
+            return result;
+        }
+    }
+}
