33cc88f0132cdfa44f80b1bef5349d7e2588298e
Dilyan-Iliev <dilqnn.ilievv@abv.bg>
2022-04-22 13:39:04 +0200

    Add tasks from fifth exercise C#-Advanced

---
+++ Level-Advanced/Exercises/Functional Programming - Exercise/P.06-Reverse And Exclude/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace P._06_Reverse_And_Exclude
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine()
+                 .Split()
+                 .Select(int.Parse)
+                 .Reverse();
+               
+
+            int numberToDivideOn = int.Parse(Console.ReadLine());
+
+            IEnumerable<int> result = RemoveElements(numbers.ToList(), x=> x % numberToDivideOn != 0);
+            //numbers.RemoveAll(x => x % numberToDivideOn == 0);
+
+            Console.WriteLine(string.Join(" ", result));
+        }
+
+        static IEnumerable<int> RemoveElements(List<int> list, Func<int, bool> filter)
+        {
+            List<int> result = new List<int>();
+            foreach (var number in list)
+            {
+                if (filter(number))
+                {
+                    result.Add(number);
+                }
+            }
+            return result;
+        }
+    }
+}
