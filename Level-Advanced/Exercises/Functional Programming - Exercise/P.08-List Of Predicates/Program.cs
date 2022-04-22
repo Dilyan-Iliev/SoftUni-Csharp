2b561f884c3603c5ca4a8c88e0f994378144a9c4
Dilyan-Iliev <dilqnn.ilievv@abv.bg>
2022-04-22 13:52:09 +0200

    Add tasks from fifth exercise C#-Advanced

---
+++ Level-Advanced/Exercises/Functional Programming - Exercise/P.08-List Of Predicates/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace P._08_List_Of_Predicates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int startOfRange = 1;
+            int endOfRange = int.Parse(Console.ReadLine());
+
+            var dividers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse);
+
+            var filteredNumbers = FilterCollection(dividers.ToList(),startOfRange, endOfRange);
+            Console.WriteLine(string.Join(" ", filteredNumbers));
+        }
+
+        static IEnumerable<int> FilterCollection(IEnumerable<int> dividers, int startOfRange, int endOfRange)
+        {
+            List<int> result = new List<int>();
+            for (int i = startOfRange; i <= endOfRange; i++)
+            {
+
+                bool isDivisible = true;
+                foreach (var divider in dividers)
+                {
+                    Func<int, bool> divideFilter = x => i % x != 0; //where x is each divider
+                    //this makes it a bool method -> example static bool Checker(int number) - return true/false
+                    if (divideFilter(divider))
+                    {
+                        isDivisible = false;
+                        break;
+                    }
+                }
+
+                if (isDivisible)
+                {
+                    result.Add(i);
+                }
+            }
+
+            return result;
+        }
+    }
+}
