e425071aee49277b130c20f496c9f95458c0cfde
Dilyan-Iliev <dilqnn.ilievv@abv.bg>
2022-04-22 13:49:13 +0200

    Add tasks from fifth exercise C#-Advanced

---
+++ Level-Advanced/Exercises/Functional Programming - Exercise/P.07-Predicate For Names/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace P._07_Predicate_For_Names
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int lengthOfName = int.Parse(Console.ReadLine());
+            var names = Console.ReadLine()
+                .Split();
+
+            Func<string, bool> predicate = name => name.Length <= lengthOfName;
+
+            var filteredNames = names.Where(predicate).ToArray();
+
+            Console.WriteLine(string.Join(Environment.NewLine, filteredNames));
+        }
+    }
+}
