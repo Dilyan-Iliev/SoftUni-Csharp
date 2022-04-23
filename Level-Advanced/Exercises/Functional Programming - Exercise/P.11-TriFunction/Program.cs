328f8f766e6167d668a891b1405be302008e0244
Dilyan-Iliev <dilqnn.ilievv@abv.bg>
2022-04-23 18:10:40 +0200

    Add tasks from fifth exercise C#-Advanced

---
+++ Level-Advanced/Exercises/Functional Programming - Exercise/P.11-TriFunction/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace P._11_TriFunction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n = int.Parse(Console.ReadLine());
+            string[] names = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            List<string> namesWithSum = new List<string>();
+
+            foreach (var name in names)
+            {
+                int nameCharsSum = 0;
+                for (int i = 0; i < name.Length; i++)
+                {
+                    char c = name[i];
+                    nameCharsSum += c;
+                }
+                if (nameCharsSum >= n)
+                {
+                    namesWithSum.Add(name);
+                }
+            }
+
+            var newList = namesWithSum.FirstOrDefault().ToList();
+            if (newList.Any())
+            {
+                Console.WriteLine(string.Join("", newList));
+            }
+        }
+    }
+}
