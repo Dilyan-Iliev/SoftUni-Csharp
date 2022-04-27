0cef909831bb9d17123bc7356e82e622e4afcf0e
Dilyan-Iliev <dilqnn.ilievv@abv.bg>
2022-04-27 16:10:28 +0200

    Add tasks from sixth lection C#-Advanced

---
+++ Level-Advanced/Labs/Defining Classes - Lab/P.03-Car Constructors/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CarManufacturer
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string carMake = Console.ReadLine();
+            string carName = Console.ReadLine();
+            int carYear = int.Parse(Console.ReadLine());
+            double fuelQuantity = double.Parse(Console.ReadLine());
+            double fuelConsumption = double.Parse(Console.ReadLine());
+
+            var firstCar = new Car();
+            var secondCar = new Car(carMake, carName, carYear);
+            var thirdCar = new Car(carMake, carName, carYear, fuelQuantity, fuelConsumption);
+        }
+    }
+}
