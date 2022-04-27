b53e69e7c6f4c53a67dc46bcfa03db46c2c1f196
Dilyan-Iliev <dilqnn.ilievv@abv.bg>
2022-04-27 16:08:38 +0200

    Add tasks from sixth lection C#-Advanced

---
+++ Level-Advanced/Labs/Defining Classes - Lab/P.01-CarManufacturer/Car.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarManufacturer
+{
+    public class Car
+    {
+        private string make;
+        private string model;
+        private int year;
+
+        public string Make
+        {
+            get { return this.make; }
+            set { this.make = value; }
+        }
+
+        public string Model
+        {
+            get { return this.model; }
+            set { this.model = value; }
+        }
+
+        public int Year
+        {
+            get { return this.year; }
+            set { this.year = value; }
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine($"Make: {this.Make}");
+            sb.AppendLine($"Model: {this.Model}");
+            sb.AppendLine($"Year: {this.Year.ToString()}");
+            return sb.ToString();
+        }
+    }
+}
