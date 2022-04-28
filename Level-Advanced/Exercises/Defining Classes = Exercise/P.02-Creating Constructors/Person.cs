a9d79bb6f29770b6d90c9392e5209332cf3a1d19
Dilyan-Iliev <dilqnn.ilievv@abv.bg>
2022-04-28 18:22:25 +0200

    Add tasks from sixth exercise C#-Advanced

---
+++ Level-Advanced/Exercises/Defining Classes = Exercise/P.02-Creating Constructors/Person.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public class Person
+    {
+        //Constructors
+        public Person()
+        {
+            this.Name = "No name";
+            this.Age = 1;
+        }
+
+        public Person(int number) : this()
+        {
+            this.Age = number;
+        }
+
+        public Person(string name, int number)  
+        {
+            this.Name = name;
+            this.Age = number;
+        }
+
+        //Fields
+        private string name;
+        private int age;
+
+        //Properties
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+
+        public int Age
+        {
+            get { return this.age; }
+            set { this.age = value; }
+        }
+    }
+}
