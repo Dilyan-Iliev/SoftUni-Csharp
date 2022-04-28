c44d330c638c8be3e1f0b75814023a36b2e7b50b
Dilyan-Iliev <dilqnn.ilievv@abv.bg>
2022-04-28 18:21:22 +0200

    Add tasks from sixth exercise C#-Advanced

---
+++ Level-Advanced/Exercises/Defining Classes = Exercise/P.01-Define a Class Person/Person.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public class Person
+    {
+        //Constructors
+        public Person()
+        {
+
+        }
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age; 
+        }
+
+        //Fields
+        private string name;
+        private int age;
+
+        //Properties
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+        public int Age
+        {
+            get { return this.age; }
+            set { this.age = value; }
+        }
+    }
+}
---
+++ Level-Advanced/Exercises/Defining Classes = Exercise/P.01-Define a Class Person/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person();
+            person.Name = "Peter";
+            person.Age = 20;
+
+            var secondPerson = new Person("George", 18);
+        }
+    }
+}
