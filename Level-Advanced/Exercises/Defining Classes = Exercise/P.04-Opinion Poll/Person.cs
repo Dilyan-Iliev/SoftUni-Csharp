6593855df958c7c0dec4ec85d08cde6d56e67e0e
Dilyan-Iliev <dilqnn.ilievv@abv.bg>
2022-04-28 18:23:59 +0200

    Add tasks from sixth exercise C#-Advanced

---
+++ Level-Advanced/Exercises/Defining Classes = Exercise/P.04-Opinion Poll/Person.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P._04_Opinion_Poll
+{
+    public class Person
+    {
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+
+        private string name;
+        private int age;
+
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+
+        public int Age
+        {
+            get { return this.age; }
+            set { this.age = value; }
+        }
+
+        public override string ToString()
+        {
+            return $"{this.Name} - {this.Age}";
+        }
+    }
+}
---
+++ Level-Advanced/Exercises/Defining Classes = Exercise/P.04-Opinion Poll/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace P._04_Opinion_Poll
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            var people = new List<Person>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] personInfo = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                string name = personInfo[0];
+                int age = int.Parse(personInfo[1]);
+
+                var guy = new Person(name, age);
+                people.Add(guy);    
+            }
+
+            foreach (var person in people.OrderBy(x => x.Name))
+            {
+                if (person.Age > 30)
+                {
+                    Console.WriteLine(person.ToString());
+                }
+            }
+        }
+    }
+}
