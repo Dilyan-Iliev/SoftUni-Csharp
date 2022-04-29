b774097d8610be1ccbb8928865986653fceb1697
Dilyan-Iliev <dilqnn.ilievv@abv.bg>
2022-04-29 14:41:46 +0200

    Add tasks from sixth exercise C#-Advanced

---
+++ Level-Advanced/Exercises/Defining Classes = Exercise/P.06-Speed Racing/Class1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P._06_Speed_Racing
+{
+    public class Car
+    {
+        public Car(string model, double fuelAmout, double fuelConsumption)
+        {
+            this.Model = model;
+            this.FuelAmount = fuelAmout;
+            this.FuelConstumptionPerKilometer = fuelConsumption;
+            this.TravelledDistance = 0;
+
+        }
+        //Fields
+        private string model;
+        private double fuelAmount;
+        private double fuelConsumptionPerKilometer;
+        private double travelledDistance;
+
+
+        //Properties
+        public string Model
+        {
+            get { return this.model; }
+            set { this.model = value; }
+        }
+        public double FuelAmount
+        {
+            get { return this.fuelAmount; }
+            set { this.fuelAmount = value; }
+        }
+        public double FuelConstumptionPerKilometer
+        {
+            get { return this.fuelConsumptionPerKilometer; }
+            set { this.fuelConsumptionPerKilometer = value; }
+        }
+        public double TravelledDistance
+        {
+            get { return this.travelledDistance; }
+            set { this.travelledDistance = value; }
+        }
+
+        public void CheckForDistance(double distance)
+        {
+            bool check = this.FuelConstumptionPerKilometer * distance <= this.FuelAmount;
+
+            if (check)
+            {
+                this.FuelAmount -= this.FuelConstumptionPerKilometer * distance;
+                TravelledDistance += distance;
+            }
+            else
+            {
+                Console.WriteLine("Insufficient fuel for the drive");
+            }
+        }
+    }
+}
---
+++ Level-Advanced/Exercises/Defining Classes = Exercise/P.06-Speed Racing/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P._06_Speed_Racing
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            var dictionary = new Dictionary<string, Car>();
+
+            for (int i = 0; i < n; i++)
+            {
+                var inputData = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                var carModel = inputData[0];
+                var carFuelAmount = double.Parse(inputData[1]);
+                var carFuelConsumptionFor1km = double.Parse(inputData[2]);
+
+
+                if (!dictionary.ContainsKey(carModel))
+                {
+                    var car = new Car(carModel, carFuelAmount, carFuelConsumptionFor1km);
+                    dictionary[carModel] = car;
+                    //it works with dictionary[car.Model] = car;
+                }
+            }
+
+            string command;
+            while ((command = Console.ReadLine()) != "End")
+            {
+                var cmdArgs = command
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                var carModel = cmdArgs[1];
+                var amountOfKm = double.Parse(cmdArgs[2]);
+
+                if (cmdArgs[0] == "Drive")
+                {
+                    dictionary[carModel].CheckForDistance(amountOfKm);
+
+                }
+            }
+
+            foreach (KeyValuePair<string, Car> keyValuePair in dictionary)
+            {
+                Console.WriteLine($"{keyValuePair.Key} {keyValuePair.Value.FuelAmount:f2} {keyValuePair.Value.TravelledDistance}");
+            }
+        }
+    }
+}
