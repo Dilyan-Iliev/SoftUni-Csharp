b0df62206398927d03a7c1cfc2c66be9613ed9a8
Dilyan-Iliev <dilqnn.ilievv@abv.bg>
2022-04-29 15:02:11 +0200

    Add tasks from sixth exercise C#-Advanced

---
+++ Level-Advanced/Exercises/Defining Classes = Exercise/P.05-Date Modifier/DateModifier.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace P._05_Date_Modifier
+{
+    public class DateModifier
+    {
+        public DateModifier(string firstDate, string secondDate)
+        {
+            this.FirstDate = DateTime.ParseExact(firstDate, "yyyy MM dd", CultureInfo.InvariantCulture);
+            this.SecondDate = DateTime.ParseExact(secondDate, "yyyy MM dd", CultureInfo.InvariantCulture);
+        }
+        private DateTime firstDate;
+        private DateTime secondDate;
+
+        public DateTime FirstDate
+        {
+            get { return this.firstDate; }  
+            set { this.firstDate = value; }
+        }
+        public DateTime SecondDate
+        {
+            get { return this.secondDate; }
+            set { this.secondDate = value; }
+        }
+        public int CalculateDifferenceBetweenDays(string firstDate, string secondDate)
+        {
+            return Math.Abs((this.FirstDate.Date - this.SecondDate.Date).Days);
+        }
+    }
+}
---
+++ Level-Advanced/Exercises/Defining Classes = Exercise/P.05-Date Modifier/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace P._05_Date_Modifier
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string firstDate = Console.ReadLine();
+            string secondDate = Console.ReadLine();
+
+            var dateModifier = new DateModifier(firstDate, secondDate);
+            Console.WriteLine(dateModifier.CalculateDifferenceBetweenDays(firstDate, secondDate));
+        }
+    }
+}
