f937e6d3e6ed8fa483174b9a8d1588e5ab13b868
Dilyan-Iliev <dilqnn.ilievv@abv.bg>
2022-05-04 17:19:46 +0200

    Add tasks from seventh lection C#-Advanced

---
+++ Level-Advanced/Labs/Generics - Lab/P.03-Generic Scale/EqualityScale.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericScale
+{
+    public class EqualityScale<T>
+        //where T : struct or where T : class depending on the restrictions we need
+    {
+        public EqualityScale(T left, T right)
+        {
+            this.Left = left;
+            this.Right = right;
+        }
+
+        private T left;
+        private T right;
+
+        public T Left
+        {
+            get { return this.left; }
+            set { this.left = value; }
+        }
+        public T Right
+        {
+            get { return this.right; }
+            set { this.right = value; }
+        }
+
+        public bool AreEqual()
+        {
+            return this.Right.Equals(this.Left);
+        }
+    }
+}
---
+++ Level-Advanced/Labs/Generics - Lab/P.03-Generic Scale/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace GenericScale
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var obj = new EqualityScale<string>("name1", "name2");
+            Console.WriteLine(obj.AreEqual());
+
+            var obj2 = new EqualityScale<int>(3, 3);
+            Console.WriteLine(obj2.AreEqual());
+        }
+    }
+}
