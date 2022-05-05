3e78738dacf85aed18362d0c1e21a55646897a1c
Dilyan-Iliev <dilqnn.ilievv@abv.bg>
2022-05-05 09:35:16 +0200

    Add tasks from seventh exercise C#-Advanced

---
+++ Level-Advanced/Exercises/Generics - Exercise/P.01-Generic Box of String/Box.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P._01_Generic_Box_of_String
+{
+    public class Box<T>
+    {
+        private T boxElement;
+
+        public T BoxElement
+        {
+            get { return this.boxElement; }
+            set { this.boxElement = value; }
+        }
+
+        public override string ToString()
+        {
+            return $"{this.BoxElement.GetType().FullName}: {this.BoxElement}";
+        }
+    }
+}
---
+++ Level-Advanced/Exercises/Generics - Exercise/P.01-Generic Box of String/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P._01_Generic_Box_of_String
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                Box<string> box = new Box<string>();
+                box.BoxElement = Console.ReadLine();
+                Console.WriteLine(box.ToString());
+            }
+        }
+    }
+}
