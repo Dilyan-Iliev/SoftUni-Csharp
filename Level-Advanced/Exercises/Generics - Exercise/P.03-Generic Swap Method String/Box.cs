df763a2d929944e362e47dcc8f9c3bda909fc131
Dilyan-Iliev <dilqnn.ilievv@abv.bg>
2022-05-05 09:37:11 +0200

    Add tasks from seventh exercise C#-Advanced

---
+++ Level-Advanced/Exercises/Generics - Exercise/P.03-Generic Swap Method String/Box.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P._03_Generic_Swap_Method_String
+{
+    public class Box<T>
+    {
+        private T element;
+
+        public T Element
+        {
+            get { return this.element; }
+            set { this.element = value; }
+        }
+        public override string ToString()
+        {
+            return $"{this.Element.GetType().FullName}: {this.Element}";
+        }
+    }
+}
---
+++ Level-Advanced/Exercises/Generics - Exercise/P.03-Generic Swap Method String/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P._03_Generic_Swap_Method_String
+{
+    public class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            var list = new List<Box<string>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string element = Console.ReadLine();
+                Box<string> box = new Box<string>();
+                box.Element = element;
+
+                list.Add(box);
+            }
+
+            int[] arr = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            SwapList(list, arr);
+            PrintList<Box<string>>(list);
+        }
+        static void PrintList<T>(List<T> list)
+        {
+            foreach (var element in list)
+            {
+                Console.WriteLine(element.ToString());
+            }
+        }
+        static List<T> SwapList<T>(List<T> list, int[] indices)
+        {
+            var firstIndex = indices[0];
+            var secondIndex = indices[1];
+
+            T currentElement = list[firstIndex];
+            list[firstIndex] = list[secondIndex];
+            list[secondIndex] = currentElement;
+
+            return list;
+        }
+
+        //or  
+        //static void Main(string[] args)
+        //{
+        //    int n = int.Parse(Console.ReadLine());
+        //    var list = new List<string>();
+
+        //    for (int i = 0; i < n; i++)
+        //    {
+        //        string element = Console.ReadLine();
+        //        list.Add(element);
+        //    }
+
+        //    int[] indices = Console.ReadLine()
+        //        .Split()
+        //        .Select(int.Parse)
+        //        .ToArray();
+
+        //    SwapList(list, indices);
+        //    PrintList<string>(list);
+        //}
+    }
+}
