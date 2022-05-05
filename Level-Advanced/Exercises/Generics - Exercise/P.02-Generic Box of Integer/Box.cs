6b3941194438f9aa8285f4a5dd69237233523162
Dilyan-Iliev <dilqnn.ilievv@abv.bg>
2022-05-05 09:36:21 +0200

    Add tasks from seventh exercise C#-Advanced

---
+++ Level-Advanced/Exercises/Generics - Exercise/P.02-Generic Box of Integer/Box.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P._02_Generic_Box_of_Integer
+{
+    public class Box<T>
+    {
+        private T elementToAdd;
+
+        public T ElementToAdd
+        {
+            get { return this.elementToAdd; }
+            set { this.elementToAdd = value; }
+        }
+
+        public override string ToString()
+        {
+            return $"{this.ElementToAdd.GetType().FullName}: {this.ElementToAdd}";
+        }
+    }
+}
---
+++ Level-Advanced/Exercises/Generics - Exercise/P.02-Generic Box of Integer/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace P._02_Generic_Box_of_Integer
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                var box = new Box<int>();
+                box.ElementToAdd = int.Parse(Console.ReadLine());
+                Console.WriteLine(box.ToString());
+            }
+        }
+    }
+}
