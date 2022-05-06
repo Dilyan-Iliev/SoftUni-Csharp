113b5fa27b3548298aca86a518b2c8dae7309fc8
Dilyan-Iliev <dilqnn.ilievv@abv.bg>
2022-05-06 14:31:14 +0200

    Add tasks from seventh exercise C#-Advanced

---
+++ Level-Advanced/Exercises/Generics - Exercise/P.06-Generic Count Method Double/Box.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P._06_Generic_Count_Method_Double
+{
+    public class Box<T>
+        where T : IComparable<T>
+    {
+        private T element;
+        public T Element
+        {
+            get { return element; }
+            set { element = value; }
+        }
+
+        public int Compare(T element)
+        {
+            return this.Element.CompareTo(element);
+        }
+    }
+}
---
+++ Level-Advanced/Exercises/Generics - Exercise/P.06-Generic Count Method Double/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P._06_Generic_Count_Method_Double
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            List<Box<double>> list = new List<Box<double>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                double element = double.Parse(Console.ReadLine());
+                Box<double> box = new Box<double>();
+                box.Element = element;
+
+                list.Add(box);
+            }
+
+            double elementForComparison = double.Parse(Console.ReadLine());
+            
+            Console.WriteLine(CompareElements(list, elementForComparison));
+        }
+
+        static int CompareElements<T>(List<Box<T>> list, T elementForComparison)
+            where T : IComparable<T>
+        {
+            var filteredList = list
+                .Where(x => x.Compare(elementForComparison) > 0);
+
+            return filteredList.Count();
+        }
+    }
+}
