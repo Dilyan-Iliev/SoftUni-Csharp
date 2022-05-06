30e61f6b0971e1303041e5d6187385bb58524cfb
Dilyan-Iliev <dilqnn.ilievv@abv.bg>
2022-05-06 14:29:52 +0200

    Add tasks from seventh exercise C#-Advanced

---
+++ Level-Advanced/Exercises/Generics - Exercise/P.04-Generic Swap Method Integer/Box.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P._04_Generic_Swap_Method_Integer
+{
+    public class Box<T>
+    {
+        private T element;
+
+        public T Element
+        {
+            get { return this.element; }
+            set { this.element = value; }
+        }
+
+        public override string ToString()
+        {
+            return $"{this.Element.GetType().FullName}: {this.Element}";
+        }
+    }
+}
---
+++ Level-Advanced/Exercises/Generics - Exercise/P.04-Generic Swap Method Integer/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P._04_Generic_Swap_Method_Integer
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int numberOfLines = int.Parse(Console.ReadLine());
+
+            var list = new List<Box<int>>();
+
+            for (int i = 0; i < numberOfLines; i++)
+            {
+                int element = int.Parse(Console.ReadLine());
+
+                Box<int> box = new Box<int>();
+                box.Element = element;
+
+                list.Add(box);  
+            }
+
+            int[] indicesForSwap = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            SwapElements(list, indicesForSwap);
+            PrintOutput(list);
+        }
+
+        static List<T> SwapElements<T>(List<T> list, int[] indicesForSwap)
+        {
+            int firstIndex = indicesForSwap[0];
+            int secondIndex = indicesForSwap[1];
+
+            T currentElement = list[firstIndex];
+            list[firstIndex] = list[secondIndex];
+            list[secondIndex] = currentElement;
+
+            return list;
+        }
+
+        static void PrintOutput<T>(List<T> list)
+        {
+            foreach (var element in list)
+            {
+                Console.WriteLine(element.ToString());
+            }
+        }
+    }
+}
