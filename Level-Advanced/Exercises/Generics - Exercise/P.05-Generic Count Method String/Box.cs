cef8199009f9270fe58ce31f02dcf3cda759e11d
Dilyan-Iliev <dilqnn.ilievv@abv.bg>
2022-05-06 14:30:32 +0200

    Add tasks from seventh exercise C#-Advanced

---
+++ Level-Advanced/Exercises/Generics - Exercise/P.05-Generic Count Method String/Box.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P._05_Generic_Count_Method_String
+{
+    public class Box<T>
+        where T : IComparable<T>
+    {
+        private T element;
+
+        public T Element
+        {
+            get { return this.element; }
+            set { this.element = value; }
+        }
+
+        public int Compare(T element)
+        {//create a method which will be used later
+
+            return this.Element.CompareTo(element);
+        }
+    }
+}
---
+++ Level-Advanced/Exercises/Generics - Exercise/P.05-Generic Count Method String/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P._05_Generic_Count_Method_String
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int numberOfLines = int.Parse(Console.ReadLine());
+            var list = new List<Box<string>>();
+
+            for (int i = 0; i < numberOfLines; i++)
+            {
+                string element = Console.ReadLine();
+                Box<string> box = new Box<string>();
+                box.Element = element;
+
+                list.Add(box);
+            }
+
+            string elementForComparison = Console.ReadLine();
+            Console.WriteLine(CompareElements(list, elementForComparison));
+        }
+
+        static int CompareElements<T>(List<Box<T>> list, T elementForComparison)
+            where T : IComparable<T>
+        {
+            var filteredList = list
+                .Where(x => x.Compare(elementForComparison) > 0);
+
+            return filteredList.Count();
+
+            //int counter = 0;
+            //foreach (var element in list)
+            //{
+            //    if (element.Compare(elementForComparison) > 0)
+            //    {
+            //        counter++;
+            //    }
+            //}
+            //return counter;
+           
+        }
+    }
+}
