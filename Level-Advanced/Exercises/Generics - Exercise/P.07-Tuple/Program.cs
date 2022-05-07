72351871c1e5135bf4abc993ff93134e54e8d741
Dilyan-Iliev <dilqnn.ilievv@abv.bg>
2022-05-07 13:39:15 +0200

    Add tasks from seventh exercise C#-Advanced

---
+++ Level-Advanced/Exercises/Generics - Exercise/P.07-Tuple/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace P._07_Tuple
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 3; i++)
+            {
+                string[] arr = Console.ReadLine()
+                    .Split();
+
+                if (i == 1)
+                {
+                    string name = $"{arr[0]} {arr[1]}";
+                    string adress = arr[2];
+                    MyTuple<string, string> tuple = new MyTuple<string, string>();
+                    tuple.FirstItem = name;
+                    tuple.SecondItem = adress;
+                    Console.WriteLine(tuple.ToString());
+                }
+                else if (i == 2)
+                {
+                    string name = arr[0];
+                    int amountOfBeer = int.Parse(arr[1]);
+                    MyTuple<string, int> tuple = new MyTuple<string, int>();
+                    tuple.FirstItem = name;
+                    tuple.SecondItem = amountOfBeer;
+                    Console.WriteLine(tuple.ToString());
+                }
+                else if (i == 3)
+                {
+                    int integer = int.Parse(arr[0]);
+                    double floatingNumber = double.Parse(arr[1]);
+                    MyTuple<int, double> tuple = new MyTuple<int, double>();
+                    tuple.FirstItem = integer;
+                    tuple.SecondItem = floatingNumber;
+                    Console.WriteLine(tuple.ToString());
+                }
+            }
+        }
+    }
+}
---
+++ Level-Advanced/Exercises/Generics - Exercise/P.07-Tuple/Tuple.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P._07_Tuple
+{
+    public class MyTuple<T1, T2>
+    {
+        private T1 firstParam;
+        private T2 secondParam;
+
+        public T1 FirstItem
+        {
+            get { return firstParam; }
+            set { firstParam = value; }
+        }
+        public T2 SecondItem
+        {
+            get { return secondParam; }
+            set { secondParam = value; }
+        }
+
+        public override string ToString()
+        {
+            return $"{this.FirstItem} -> {this.SecondItem}";
+        }
+    }
+}
