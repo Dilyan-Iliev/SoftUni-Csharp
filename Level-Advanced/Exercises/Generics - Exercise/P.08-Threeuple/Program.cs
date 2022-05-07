95d9a83342ddd8935315f17f4f460a675cbcb916
Dilyan-Iliev <dilqnn.ilievv@abv.bg>
2022-05-07 13:39:49 +0200

    Add tasks from seventh exercise C#-Advanced

---
+++ Level-Advanced/Exercises/Generics - Exercise/P.08-Threeuple/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace P._08_Threeuple
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 3; i++)
+            {
+                string[] arr = Console.ReadLine()
+                    .Split(' ');
+
+                if(i == 1)
+                {
+                    string firstAndLastName = $"{arr[0]} {arr[1]}";
+                    string adress = arr[2];
+                    string town = string.Empty;
+                    if (arr.Length > 4)
+                    {
+                        town = $"{arr[3]} {arr[4]}";
+                    }
+                    else
+                    {
+                        town = arr[3];
+                    }
+
+                    Threeuple<string, string, string> threeuple = new Threeuple<string, string, string>();
+                    threeuple.FirstParam = firstAndLastName;
+                    threeuple.SecondParam = adress;
+                    threeuple.ThirdParam = town;
+                    Console.WriteLine(threeuple.ToString());
+                    
+                }
+                else if (i == 2)
+                {
+                    string name = arr[0];
+                    int liters = int.Parse(arr[1]);
+                    bool isDrunk = arr[2] == "drunk" ? true : false;
+
+                    Threeuple<string, int, bool> threeuple = new Threeuple<string, int, bool>();
+                    threeuple.FirstParam = name;
+                    threeuple.SecondParam = liters;
+                    threeuple.ThirdParam = isDrunk;
+                    Console.WriteLine(threeuple.ToString());
+                }
+                else if (i == 3)
+                {
+                    string name = arr[0];
+                    double accountBalance = double.Parse(arr[1]);
+                    string bankName = arr[2];
+
+                    Threeuple<string, double, string> threeuple = new Threeuple<string, double, string>();
+                    threeuple.FirstParam = name;
+                    threeuple.SecondParam = accountBalance;
+                    threeuple.ThirdParam = bankName;
+                    Console.WriteLine(threeuple.ToString());
+                }
+            }
+        }
+    }
+}
---
+++ Level-Advanced/Exercises/Generics - Exercise/P.08-Threeuple/Threeuple.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P._08_Threeuple
+{
+    public class Threeuple<T1, T2, T3>
+    {
+        private T1 firstParam;
+        private T2 secondParam;
+        private T3 thirdParam;
+
+        public T1 FirstParam
+        {
+            get { return firstParam; }
+            set { firstParam = value; }
+        }
+        public T2 SecondParam
+        {
+            get { return secondParam; }
+            set { secondParam = value; }
+        }
+        public T3 ThirdParam
+        {
+            get { return thirdParam; }
+            set { thirdParam = value; }
+        }
+
+        public override string ToString()
+        {
+            return $"{this.FirstParam} -> {this.SecondParam} -> {this.ThirdParam}";
+        }
+    }
+}
