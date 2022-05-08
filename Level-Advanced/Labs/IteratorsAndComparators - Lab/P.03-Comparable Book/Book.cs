95a2a117314e744300bf03e72dacc3f1b302cba8
Dilyan-Iliev <dilqnn.ilievv@abv.bg>
2022-05-08 17:17:06 +0200

    Add tasks from eighth lection C#-Advanced

---
+++ Level-Advanced/Labs/IteratorsAndComparators - Lab/P.03-Comparable Book/Book.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+
+namespace IteratorsAndComparators
+{
+    public class Book : IComparable<Book>
+    {
+        public Book(string title, int year, params string[] authors)
+        {
+            this.Title = title;
+            this.Year = year;
+            this.Authors = authors.ToList();
+        }
+        private string title;
+        private int year;
+        private List<string> authors;
+
+        public string Title
+        {
+            get { return this.title; }
+            set { this.title = value; }
+        }
+        public int Year
+        {
+            get { return this.year; }
+            set { this.year = value; }
+        }
+        public List<string> Authors
+        {
+            get { return this.authors; }
+            set { this.authors = value; }
+        }
+
+        public int CompareTo(Book other)
+        {
+            if (this.Year.CompareTo(other.Year) > 0)
+            {
+                return 1;
+            }
+            else if (this.Year.CompareTo(other.Year) < 0)
+            {
+                return -1;
+            }
+            else // if this.Year.CompareTo(other.Year) == 0)
+            {
+                if (this.Title.CompareTo(other.Title) > 0)
+                {
+                    return 1;
+                }
+                else if (this.Title.CompareTo(other.Title) < 0)
+                {
+                    return -1;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+        }
+        public override string ToString()
+        {
+            return $"{this.Title} - {this.Year}";
+        }
+    }
+}
---
+++ Level-Advanced/Labs/IteratorsAndComparators - Lab/P.03-Comparable Book/LibraryIterator.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IteratorsAndComparators
+{
+    public class LibraryIterator : IEnumerator<Book>
+    {
+        private List<Book> books;
+        private int currentIndex;
+        public LibraryIterator(IEnumerable<Book> books)
+        {
+            this.books = new List<Book>(books);
+            this.currentIndex = -1;
+        }
+        public Book Current => this.books[currentIndex];
+
+        object IEnumerator.Current => this.Current;
+
+        public void Dispose()
+        {
+
+        }
+
+        public bool MoveNext()
+        {
+            this.currentIndex++;
+            if (this.currentIndex < this.books.Count)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void Reset()
+        {
+            this.currentIndex = -1;
+        }
+    }
+}
