09486fd5000d13c47cc23b04a640acfd0389efa3
Dilyan-Iliev <dilqnn.ilievv@abv.bg>
2022-05-09 14:49:27 +0200

    Add tasks from eighth exercise C#-Advanced

---
+++ Level-Advanced/Exercises/IteratorsAndComparators - Exercise/P.02-Collection/ListyIterator.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P._02_Collection
+{
+    public class ListyIterator<T> : IEnumerable<T>
+    {
+        public ListyIterator(params T[] parameters)
+        {
+            this.list = new List<T>(parameters);
+            this.index = 0;
+        }
+        private int index;
+        private List<T> list;
+
+        public bool Move()
+        {
+
+            if (index < list.Count - 1)
+            {
+                index++;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public bool HasNext()
+        {
+            if (index + 1 < list.Count)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public void Print()
+        {
+            if (list.Count == 0)
+            {
+                Console.WriteLine("Invalid Operation!");
+            }
+            else
+            {
+                Console.WriteLine(list[index]);
+            }
+        }
+        public void PrintAll()
+        {
+            Console.WriteLine(string.Join(" ", list));
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < list.Count; i++)
+            {
+                yield return list[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}   
---
+++ Level-Advanced/Exercises/IteratorsAndComparators - Exercise/P.02-Collection/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+
+namespace P._02_Collection
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            ListyIterator<string> obj = null;
+
+            string command;
+            while ((command = Console.ReadLine()) != "END")
+            {
+                string[] cmdArgs = command
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                string currentCmd = cmdArgs[0]; 
+
+                if (currentCmd == "Create")
+                {
+                    obj = new ListyIterator<string>(cmdArgs.Skip(1).ToArray());
+                }
+                else if (currentCmd == "Move")
+                {
+                    Console.WriteLine(obj.Move());
+                }
+                else if (currentCmd == "HasNext")
+                {
+                    Console.WriteLine(obj.HasNext());
+                }
+                else if (currentCmd == "Print")
+                {
+                    obj.Print();
+                }
+                else if (currentCmd == "PrintAll")
+                {
+                    obj.PrintAll();
+                }
+            }
+        }
+    }
+}
