9c90bda1c82bced962bcc35c5e4588984c47fb6f
Dilyan-Iliev <dilqnn.ilievv@abv.bg>
2022-05-09 14:49:39 +0200

    Add tasks from eighth exercise C#-Advanced

---
+++ Level-Advanced/Exercises/IteratorsAndComparators - Exercise/P.03-Stack/CustomClass.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace P._03_Stack
+{
+    public class CustomClass<T> : IEnumerable<T>
+    {
+        public CustomClass(params T[] elements)
+        {
+            this.list = new List<T>();
+            this.index = -1;
+        }
+        private int index;
+        private List<T> list;
+
+        public void Push(params T[] elements)
+        {
+            foreach (var element in elements)
+            {
+                index++;
+                list.Insert(index, element);
+            }
+        }
+        public void Pop()
+        {
+            if (list.Count == 0)
+            {
+                Console.WriteLine("No elements");
+            }
+            else
+            {
+                list.RemoveAt(index);
+            }
+            index--;
+
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = list.Count - 1; i >= 0; i--)
+            {
+                yield return list[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
---
+++ Level-Advanced/Exercises/IteratorsAndComparators - Exercise/P.03-Stack/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+
+namespace P._03_Stack
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var obj = new CustomClass<string>();
+
+            string command;
+            while ((command = Console.ReadLine()) != "END")
+            {
+                string[] cmdArgs = command
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                string currentCmd = cmdArgs[0];
+
+                if (currentCmd == "Push")
+                {
+                    string[] elements = cmdArgs
+                        .Skip(1)
+                        .Select(x => x.Split(",").First())
+                        .ToArray();
+
+                    obj.Push(elements);
+                }
+                else if (currentCmd == "Pop")
+                {
+                    obj.Pop();
+                }
+            }
+
+            foreach (var element in obj)
+            {
+                Console.WriteLine(element);
+            }
+            foreach (var element in obj)
+            {
+                Console.WriteLine(element);
+            }
+        }
+    }
+}
