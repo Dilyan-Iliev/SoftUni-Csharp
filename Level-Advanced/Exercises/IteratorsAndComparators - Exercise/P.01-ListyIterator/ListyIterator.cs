44078cc9c9e314ca6284a5a10e3890c44ebb5fae
Dilyan-Iliev <dilqnn.ilievv@abv.bg>
2022-05-09 14:49:12 +0200

    Add tasks from eighth exercise C#-Advanced

---
+++ Level-Advanced/Exercises/IteratorsAndComparators - Exercise/P.01-ListyIterator/ListyIterator.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P._01_ListyIterator
+{
+    public class ListyIterator<T>
+    {
+        public ListyIterator(params T[] parameters)
+        {
+            this.list = new List<T>(parameters);
+            this.index = 0;
+        }
+        private int index;
+        private List<T> list;
+
+        public bool Move()
+        {
+            
+            if (index < list.Count-1)
+            {
+                index++;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public bool HasNext()
+        {
+            if (index + 1 < list.Count)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public void Print()
+        {
+            if (list.Count == 0)
+            {
+                Console.WriteLine("Invalid Operation!");
+            }
+            else
+            {
+                Console.WriteLine(list[index]);
+            }
+        }
+    }
+}
---
+++ Level-Advanced/Exercises/IteratorsAndComparators - Exercise/P.01-ListyIterator/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+
+namespace P._01_ListyIterator
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            ListyIterator<string> obj = null;
+
+            string command;
+            while ((command = Console.ReadLine()) != "END")
+            {
+                string[] cmdArgs = command
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                string currentCmd = cmdArgs[0];
+
+                if (currentCmd == "Create")
+                {
+                    string[] elementsForTheObj = cmdArgs.Skip(1).ToArray();
+                    obj = new ListyIterator<string>(elementsForTheObj);
+                }
+                else if (currentCmd == "Move")
+                {
+                    Console.WriteLine(obj.Move());
+                }
+                else if (currentCmd == "HasNext")
+                {
+                    Console.WriteLine(obj.HasNext());
+                }
+                else if (currentCmd == "Print")
+                {
+                    obj.Print();
+                }
+            }
+        }
+    }
+}
