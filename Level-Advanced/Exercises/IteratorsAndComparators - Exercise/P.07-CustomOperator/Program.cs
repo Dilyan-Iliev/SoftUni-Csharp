2caca618f02f14ec555d509c3764cc64a970a798
Dilyan-Iliev <dilqnn.ilievv@abv.bg>
2022-05-10 09:03:52 +0200

    Add tasks from eighth exercise C#-Advanced

---
+++ Level-Advanced/Exercises/IteratorsAndComparators - Exercise/P.07-CustomOperator/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P._07_CustomOperator
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+
+            var evenFiltered = numbers.Where(x => x % 2 == 0).OrderBy(x => x).ToArray();
+            var oddFilter = numbers.Where(x => x % 2 != 0).OrderBy(x => x).ToArray();
+
+            var mergedCollection = evenFiltered.Concat(oddFilter).ToArray();
+
+            Console.WriteLine(string.Join(" ", mergedCollection));
+        }
+    }
+}
