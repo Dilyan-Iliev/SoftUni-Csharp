89b3559cd5990d997d5cefd7527410ed58fd4ff9
Dilyan-Iliev <dilqnn.ilievv@abv.bg>
2022-05-10 09:03:23 +0200

    Add tasks from eighth exercise C#-Advanced

---
+++ Level-Advanced/Exercises/IteratorsAndComparators - Exercise/P.04-Froggy/Lake.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P._04_Froggy
+{
+    public class Lake : IEnumerable<int>
+    {
+        public Lake(int[] stones)
+        {
+            this.PathOfStones = stones;
+        }
+        private int[] pathOfStones;
+
+        public int[] PathOfStones
+        {
+            get { return pathOfStones; }
+            set { pathOfStones = value; }
+        }
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            for (int i = 0; i < pathOfStones.Length; i+=2)
+            {
+                yield return pathOfStones[i];
+            }
+
+
+            int startingReversedIndex = 0;
+
+            if (pathOfStones.Length % 2 == 0)
+            {
+                startingReversedIndex = pathOfStones.Length - 1;
+            }
+            else
+            {
+                startingReversedIndex = pathOfStones.Length - 2;
+            }
+
+            for (int i = startingReversedIndex; i > 0; i-=2)
+            {
+                yield return pathOfStones[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ Level-Advanced/Exercises/IteratorsAndComparators - Exercise/P.04-Froggy/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace P._04_Froggy
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] stones = Console.ReadLine()
+                .Split(", ",StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            var lake = new Lake(stones);
+            Console.WriteLine(string.Join(", ", lake));
+        }
+    }
+}
