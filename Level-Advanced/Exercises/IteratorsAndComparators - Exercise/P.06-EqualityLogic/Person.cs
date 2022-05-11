eaa01ca4cdd5ab680161fa846ee7d52cd904eafc
Dilyan-Iliev <dilqnn.ilievv@abv.bg>
2022-05-11 10:33:11 +0200

    Add tasks from eighth exercise C#-Advanced

---
+++ Level-Advanced/Exercises/IteratorsAndComparators - Exercise/P.06-EqualityLogic/Person.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace P._06_EqualityLogic
+{
+    public class Person : IComparable<Person>
+    {
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+
+        private string name;
+        private int age;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+
+        public int CompareTo(Person other)
+        {
+            int result = this.Name.CompareTo(other.Name);
+
+            if (result == 0)
+            {
+                result = this.Age.CompareTo(other.Age);
+            }
+
+            return result;
+        }
+
+        public override int GetHashCode()
+        {
+            return this.Name.GetHashCode() ^ this.Age.GetHashCode();
+            // ^ - знак за сравнение
+        }
+        public override bool Equals(object? obj)
+        {
+            var other = obj as Person;
+
+            if (other == null)
+            {
+                return false;
+            }
+
+            return this.Age == other.Age && this.Name == other.Name;
+        }
+
+        public override string ToString()
+        {
+            return $"{this.Name} {this.Age}";
+        }
+    }
+}
---
+++ Level-Advanced/Exercises/IteratorsAndComparators - Exercise/P.06-EqualityLogic/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P._06_EqualityLogic
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            var hashset = new HashSet<Person>();
+            var sortedSed = new SortedSet<Person>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                string name = input[0];
+                int age = int.Parse(input[1]);
+
+                var person = new Person(name, age);
+
+                hashset.Add(person); //прави GetHashCode  
+                sortedSed.Add(person); //прави Equals     
+            }
+
+            Console.WriteLine(sortedSed.Count);
+            Console.WriteLine(hashset.Count);
+        }
+    }
+}
