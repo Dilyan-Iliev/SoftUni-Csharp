7e717b02ccbf63125e105807a892414854777ec3
Dilyan-Iliev <dilqnn.ilievv@abv.bg>
2022-05-20 10:41:52 +0200

    Add tasks from Exam from 14.04.2021

---
+++ Level-Advanced/Exams/Exam from 14.04.2021/P.01-WarmWinter/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace P._01_WarmWinter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] hats = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int[] scarfs = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Stack<int> hatsStack = new Stack<int>(hats);
+            Queue<int> scarfsQueue = new Queue<int>(scarfs);
+            List<int> sets = new List<int>();
+
+            while (hatsStack.Any() && scarfsQueue.Any())
+            {
+                int currentHat = hatsStack.Peek();
+                int currentScarf = scarfsQueue.Peek();
+
+                if (currentHat > currentScarf)
+                {
+                    int setPrice = currentHat + currentScarf;
+                    sets.Add(setPrice);
+
+                    hatsStack.Pop();
+                    scarfsQueue.Dequeue();
+                }
+                else if (currentScarf > currentHat)
+                {
+                    hatsStack.Pop();
+                }
+                else if (currentHat == currentScarf)
+                {
+                    scarfsQueue.Dequeue();
+
+                    var hat = hatsStack.Pop() + 1;
+                    hatsStack.Push(hat);
+                }
+            }
+
+            Console.WriteLine($"The most expensive set is: {sets.Max()}");
+            Console.WriteLine(string.Join(" ", sets));
+        }
+    }
+}
