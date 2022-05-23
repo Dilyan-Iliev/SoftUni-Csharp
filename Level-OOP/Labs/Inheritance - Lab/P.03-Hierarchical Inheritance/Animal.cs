a2d05838fdb6e2933df11aff63b6f1c9fec93467
Dilyan-Iliev <dilqnn.ilievv@abv.bg>
2022-05-23 10:25:57 +0200

    Add tasks from first lection C#-OOP

---
+++ Level-OOP/Labs/Inheritance - Lab/P.03-Hierarchical Inheritance/Animal.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P._03_Hierarchical_Inheritance
+{
+    public class Animal
+    {
+        public void Eat()
+        {
+            Console.WriteLine("eating...");
+        }
+    }
+}
---
+++ Level-OOP/Labs/Inheritance - Lab/P.03-Hierarchical Inheritance/Dog.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P._03_Hierarchical_Inheritance
+{
+    public class Dog : Animal
+    {
+        public void Bark()
+        {
+            Console.WriteLine("barking...");
+        }
+    }
+}
