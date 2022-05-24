8c551bce7d415216f96595918c7b1f5e1e8e0947
Dilyan-Iliev <dilqnn.ilievv@abv.bg>
2022-05-24 12:15:59 +0200

    Add tasks from first lection C#-OOP

---
+++ Level-OOP/Labs/Inheritance - Lab/P.05-StackOfStrings/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P._05_StackOfStrings
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            StackOfStrings stackOfStrings = new StackOfStrings();
+            Console.WriteLine(stackOfStrings.IsEmpty());
+            List<string> argsList = new List<string>();
+            argsList.Add("1");
+            argsList.Add("2");
+            argsList.Add("3");
+            stackOfStrings.AddRange(argsList);
+            Console.WriteLine(stackOfStrings.IsEmpty());
+
+        }
+    }
+}
---
+++ Level-OOP/Labs/Inheritance - Lab/P.05-StackOfStrings/StackOfStrings.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P._05_StackOfStrings
+{
+    public class StackOfStrings : Stack<string>
+    {
+        public bool IsEmpty()
+        {
+            return this.Count == 0;
+        }
+
+        public Stack<string> AddRange(IEnumerable<string> collectionToInsert)
+        {
+            foreach (var element in collectionToInsert)
+            {
+                this.Push(element);
+            }
+
+            return this;
+        }
+    }
+}
