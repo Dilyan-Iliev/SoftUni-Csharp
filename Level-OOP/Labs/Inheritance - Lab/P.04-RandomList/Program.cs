064d081a1e9ec6e8a4a8b2d90fd53dd9af03c8d3
Dilyan-Iliev <dilqnn.ilievv@abv.bg>
2022-05-24 12:15:21 +0200

    Add tasks from first lection C#-OOP

---
+++ Level-OOP/Labs/Inheritance - Lab/P.04-RandomList/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace P._04_RandomList
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            RandomList list = new RandomList();
+            list.Add("1");
+            list.Add("2");
+            list.Add("3");
+            list.Add("4");
+
+            Console.WriteLine(list.RandomString());
+            Console.WriteLine(list.RemoveElement());
+        }
+    }
+}
---
+++ Level-OOP/Labs/Inheritance - Lab/P.04-RandomList/RandomList.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P._04_RandomList
+{
+    public class RandomList : List<string>
+    {
+        private Random random;
+
+        public RandomList()
+        {
+            this.random = new Random();
+        }
+        public string RandomString()
+        {
+            string element = random.Next(0, this.Count).ToString();
+
+            return element;
+        }
+        public string RemoveElement()
+        {
+            var index = random.Next(0, this.Count);
+            string element = this[index];
+
+            this.RemoveAt(index);
+
+            return element;
+        }
+    }
+}
