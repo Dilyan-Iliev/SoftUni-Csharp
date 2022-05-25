ce5ef0edd9476e77904c35b6830ebb74ea8ad036
Dilyan-Iliev <dilqnn.ilievv@abv.bg>
2022-05-25 10:08:40 +0200

    Add tasks from first exercise C#-OOP

---
+++ Level-OOP/Exercises/Inheritance - Exercise/P.02-Zoo/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Zoo
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Animal animal = new Animal("Pesho");
+            Bear bear = new Bear("Yordan");
+            
+        }
+    }
+}
