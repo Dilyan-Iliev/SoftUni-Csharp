9ab9af70ea68fab398e7ab0b65a840bb18359055
Dilyan-Iliev <dilqnn.ilievv@abv.bg>
2022-05-30 12:38:10 +0200

    Add tasks from second lection C#-OOP

---
+++ Level-OOP/Labs/Encapsulation - Lab/P.03-Validation/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PersonsInfo
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            var people = new List<Person>();
+
+            for (int i = 0; i < n; i++)
+            {
+                var input = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                string firstName = input[0];
+                string lastName = input[1];
+                int age = int.Parse(input[2]);
+                decimal salary = decimal.Parse(input[3]);
+
+                Person person = new Person(firstName, lastName, age, salary);
+                people.Add(person);
+            }
+
+            decimal percentage = decimal.Parse(Console.ReadLine());
+
+            people.ForEach(p => p.IncreaseSalary(percentage));
+            people.ForEach(p => Console.WriteLine(p.ToString()));
+        }
+    }
+}
