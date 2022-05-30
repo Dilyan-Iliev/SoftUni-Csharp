e536ebffaec8bd861978a6f7e50eab872969ffe8
Dilyan-Iliev <dilqnn.ilievv@abv.bg>
2022-05-30 12:37:38 +0200

    Add tasks from second lection C#-OOP

---
+++ Level-OOP/Labs/Encapsulation - Lab/P.01-Sort Persons by Name and Age/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P._01_Sort_Persons_by_Name_and_Age
+{
+    public class Person
+    {
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public int Age { get; private set; }
+
+        public Person(string firstName, string lastName, int age)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+        }
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName} is {Age} years old.";
+        }
+    }
+}
---
+++ Level-OOP/Labs/Encapsulation - Lab/P.01-Sort Persons by Name and Age/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P._01_Sort_Persons_by_Name_and_Age
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            var people = new List<Person>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                string firstName = input[0];
+                string lastName = input[1];
+                int age = int.Parse(input[2]);
+
+                Person person = new Person(firstName, lastName, age);
+                people.Add(person);
+            }
+
+            foreach (var person in people.OrderBy(x => x.FirstName)
+                .ThenBy(x => x.Age))
+            {
+                Console.WriteLine(person.ToString());
+            }
+        }
+    }
+}
