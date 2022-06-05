89868f9b68473ea3f856684d1e75e464f58b9d29
Dilyan-Iliev <dilqnn.ilievv@abv.bg>
2022-06-05 17:34:02 +0200

    Add tasks from second exercise C#-OOP

---
+++ Level-OOP/Exercises/Encapsulation - Exercise/P.01-ClassBoxData/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace P._01_ClassBoxData
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            double length = double.Parse(Console.ReadLine());
+            double width = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+
+            try
+            {
+                Box box = new Box(length, width, height);
+                var surfaceArea = box.SurfaceArea();
+                var lateralSurfaceArea = box.LateralSurfaceArea();
+                var volume = box.Volume();
+
+                Console.WriteLine($"Surface Area - {surfaceArea:f2}");
+                Console.WriteLine($"Lateral Surface Area - {lateralSurfaceArea:f2}");
+                Console.WriteLine($"Volume - {volume:f2}");
+            }
+            catch (ArgumentException ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+    }
+}
