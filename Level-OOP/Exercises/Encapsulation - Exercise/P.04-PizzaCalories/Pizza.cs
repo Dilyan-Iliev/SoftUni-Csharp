828dfa6e047df353343fa813764ecd02491d93f8
Dilyan-Iliev <dilqnn.ilievv@abv.bg>
2022-06-08 11:30:16 +0200

    Add tasks from second exercise C#-OOP

---
+++ Level-OOP/Exercises/Encapsulation - Exercise/P.04-PizzaCalories/Pizza.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P._04_PizzaCalories
+{
+    public class Pizza
+    {
+        private string name;
+        private Dough dough;
+        private List<Topping> toppings;
+        public Pizza(string name, Dough dough)
+        {
+            this.Name = name;
+            this.Dough = dough;
+
+            this.toppings = new List<Topping>();
+        }
+
+        public string Name
+        {
+            get => this.name;
+            private set
+            {
+                if (value.Length < 1 || value.Length > 15)
+                {
+                    throw new ArgumentException("Pizza name should be between 1 and 15 symbols.");
+                }
+
+                this.name = value;
+            }
+        }
+
+        public Dough Dough
+        {
+            get => this.dough;
+            private set { this.dough = value; }
+        }
+
+        public IReadOnlyCollection<Topping> Toppings => this.toppings; 
+                                                     //ако някой отвън се опита да достъпи Toppings,
+                                                     //няма да може да му каже Add, защото е ReadOnly,
+                                                     //ако иска да добавя ще трябва да мине през нашия метод AddTopping
+                                                     //където имаме валидация
+        
+
+        public void AddTopping(Topping topping)
+        {
+            if (toppings.Count == 10) 
+            {
+                throw new InvalidOperationException("Number of toppings should be in range [0..10].");
+            }
+
+            this.toppings.Add(topping);
+        }
+        
+        public double CalculateTotalCalories()
+        {
+            double result = this.Dough.Calories + this.toppings.Sum(x => x.Calories);
+
+            return result;
+        }
+
+        public override string ToString()
+        {
+            return $"{this.Name} - {this.CalculateTotalCalories():f2} Calories.";
+        }
+    }
+}
---
+++ Level-OOP/Exercises/Encapsulation - Exercise/P.04-PizzaCalories/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace P._04_PizzaCalories
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string[] pizzaInfo = Console.ReadLine()
+                .Split();
+
+                string[] doughInfo = Console.ReadLine()
+                    .Split();
+
+                string pizzaName = pizzaInfo[1];
+                string doughFlourType = doughInfo[1];
+                string bakingTechnique = doughInfo[2];
+                double doughWeight = double.Parse(doughInfo[3]);
+
+                Dough dough = new Dough(doughFlourType, bakingTechnique, doughWeight);
+                Pizza pizza = new Pizza(pizzaName, dough);
+
+                string toppingType = Console.ReadLine();
+
+                while (toppingType != "END")
+                {
+                    string[] toppingInfo = toppingType
+                        .Split();
+
+                    string type = toppingInfo[1];
+                    double toppingWeight = double.Parse(toppingInfo[2]);
+
+                    Topping topping = new Topping(type, toppingWeight);
+                    pizza.AddTopping(topping);
+
+                    toppingType = Console.ReadLine();
+                }
+
+                Console.WriteLine(pizza.ToString());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
