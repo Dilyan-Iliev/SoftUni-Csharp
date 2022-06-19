34d7e300b91c2da548408f29fdb5a3edc08c5ad0
Dilyan-Iliev <dilqnn.ilievv@abv.bg>
2022-06-19 11:52:43 +0200

    Add tasks from fourth lection C#-OOP

---
+++ Level-OOP/Labs/Polymorphism - Lab/P.03-Shapes/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Shapes
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rectangle = new Rectangle(5.4, 1.2);
+            Console.WriteLine(rectangle.CalculateArea());
+            Console.WriteLine(rectangle.CalculatePerimeter());
+            Console.WriteLine(rectangle.Draw());
+
+            Circle circle = new Circle(4.2);
+            Console.WriteLine(circle.CalculateArea());
+            Console.WriteLine(circle.CalculatePerimeter());
+            Console.WriteLine(circle.Draw());
+        }
+    }
+}
