7dda2aa960348dcf7ab66ea3bfb125fef1fb69b2
Dilyan-Iliev <dilqnn.ilievv@abv.bg>
2022-06-19 11:52:15 +0200

    Add tasks from fourth lection C#-OOP

---
+++ Level-OOP/Labs/Polymorphism - Lab/P.01-MathOperations/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Operations
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            MathOperations operations = new MathOperations();
+            Console.WriteLine(operations.Add(3, 4));
+            Console.WriteLine(operations.Add(1.5, 3.5, 5.1));
+            Console.WriteLine(operations.Add(1.5m, 3.5m, 5.1m));
+        }
+    }
+}
