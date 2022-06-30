5d447cfaa54c0fbd13f92627ffe551c01d0847f4
Dilyan-Iliev <dilqnn.ilievv@abv.bg>
2022-06-30 12:50:16 +0200

    Add tasks from fifth lection C#-OOP

---
+++ Level-OOP/Labs/Exception Handling - Lab/P.01-SquareRoot/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace P._01_SquareRoot
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            try
+            {
+                int number = int.Parse(Console.ReadLine());
+                if (number <= 0 )
+                {
+                    throw new ArgumentException();
+                }
+
+                Console.WriteLine(Math.Sqrt(number));
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine("Invalid number!");
+            }
+            finally
+            {
+                Console.WriteLine("Goodbye.");
+            }
+        }
+    }
+}
