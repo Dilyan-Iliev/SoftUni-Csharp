3132018480401b5c72c5ac9a9ab540b20c5f02a5
Dilyan-Iliev <dilqnn.ilievv@abv.bg>
2022-07-01 09:22:57 +0200

    Add tasks from fifth lection-OOP

---
+++ Level-OOP/Labs/Exception Handling - Lab/P.04-FixingVol2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace P._04_FixingVol2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber, secondNumber;
+            int result;
+
+            firstNumber = 30;
+            secondNumber = 60;
+            result = Convert.ToInt32(firstNumber * secondNumber);
+            Console.WriteLine($"{firstNumber} x {secondNumber} = {result}");
+            
+        }
+    }
+}
