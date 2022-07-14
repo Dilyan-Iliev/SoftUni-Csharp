dbc8efd8aefd92770e1a66003fd6a367c9dca23d
Dilyan-Iliev <dilqnn.ilievv@abv.bg>
2022-07-14 11:00:10 +0200

    Add tasks from seventh lection C#-OOP

---
+++ Level-OOP/Labs/Reflection and Attributes - Lab/P.02-HighQualityMistakes/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Stealer
+{
+    using System;
+
+    public class Program
+    {
+        static void Main()
+        {
+            Spy spy = new Spy();
+            string result =spy.AnalyzeAccessModifiers("Hacker");
+            Console.WriteLine(result);
+        }
+    }
+}
