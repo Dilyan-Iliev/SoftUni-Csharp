abb91be9c46be2576cfd551e03f6ee380361f24a
Dilyan-Iliev <dilqnn.ilievv@abv.bg>
2022-07-15 10:42:18 +0200

    Add tasks from seventh lection C#-OOP

---
+++ Level-OOP/Labs/Reflection and Attributes - Lab/P.06-CodeTracker/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace AuthorProblem
+{
+    using System;
+
+    [Author("Dilyan")]
+    public class StartUp
+    {
+        [Author("Dilyan")]
+        static void Main(string[] args)
+        {
+            var tracker = new Tracker();
+            tracker.PrintMethodsByAuthor();
+
+        }
+    }
+}
