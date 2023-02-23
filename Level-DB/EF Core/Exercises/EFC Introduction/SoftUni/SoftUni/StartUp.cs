fa3418dad7c29bfcefc96e428da00aa887de5dda
Dilyan-Iliev <dilqnn.ilievv@abv.bg>
2023-02-23 10:28:33 +0200

    Add tasks from first exercise - EntityFramework Core

---
+++ Level-DB/EF Core/Exercises/EFC Introduction/SoftUni/SoftUni/StartUp.cs
@@ -0,0 +1,67 @@
+﻿namespace SoftUni
+{
+    using Microsoft.EntityFrameworkCore;
+    using SoftUni.Data;
+    using System.Text;
+
+    public class StartUp
+    {
+        static void Main()
+        {
+            SoftUniContext context = new SoftUniContext();
+
+            //execute desired method:
+            Console.WriteLine(GetEmployeesFullInformation(context));
+        }
+
+        //Task.03
+        public static string GetEmployeesFullInformation(SoftUniContext context)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            var employees = context.Employees
+                .AsNoTracking()
+                .OrderBy(e => e.EmployeeId)
+                .Select(e => new
+                {
+                    e.FirstName,
+                    e.LastName,
+                    e.MiddleName,
+                    e.JobTitle,
+                    Salary = $"{e.Salary:f2}"
+                })
+                .ToList();
+
+            foreach (var emp in employees)
+            {
+                sb.AppendLine($"{emp.FirstName} {emp.LastName} {emp.MiddleName} {emp.JobTitle} {emp.Salary}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+
+        //Task.04
+        public static string GetEmployeesWithSalaryOver50000(SoftUniContext context)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            var employees = context.Employees
+                .AsNoTracking()
+                .Where(e => e.Salary > 50000)
+                .OrderBy(e => e.FirstName)
+                .Select(e => new
+                {
+                    e.FirstName,
+                    Salary = $"{e.Salary:f2}"
+                })
+                .ToList();
+
+            foreach (var emp in employees)
+            {
+                sb.AppendLine($"{emp.FirstName} - {emp.Salary}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
