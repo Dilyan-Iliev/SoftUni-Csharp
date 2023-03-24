7fc4d0777b3a8773f0974c1116db28aaceefc7c0
Dilyan-Iliev <dilqnn.ilievv@abv.bg>
2023-03-24 09:55:27 +0200

    Add tasks from the exam

--- Level-DB/EF Core/Exams/08.08.2020/VaporStore/DataProcessor/Serializer.cs
+++ Level-DB/EF Core/Exams/08.08.2020/VaporStore/DataProcessor/Serializer.cs
@@ -1,17 +1,98 @@
 ﻿namespace VaporStore.DataProcessor
-{ 
+{
     using Data;
+    using Newtonsoft.Json;
+    using System.Globalization;
+    using System.Text;
+    using System.Xml.Serialization;
+    using VaporStore.Data.Models.Enums;
+    using VaporStore.DataProcessor.ExportDto;
 
     public static class Serializer
     {
         public static string ExportGamesByGenres(VaporStoreDbContext context, string[] genreNames)
         {
-            return "TODO";
+            var gamesByGenres = context
+                .Genres
+                .ToArray()
+                .Where(g => genreNames.Contains(g.Name))
+                .Select(g => new
+                {
+                    Id = g.Id,
+                    Genre = g.Name,
+                    Games = g.Games
+                        .Where(ga => ga.Purchases.Any())
+                        .Select(ga => new
+                        {
+                            Id = ga.Id,
+                            Title = ga.Name,
+                            Developer = ga.Developer.Name,
+                            Tags = String.Join(", ", ga.GameTags
+                                .Select(gt => gt.Tag.Name)
+                                .ToArray()),
+                            Players = ga.Purchases.Count
+                        })
+                        .OrderByDescending(ga => ga.Players)
+                        .ThenBy(ga => ga.Id)
+                        .ToArray(),
+                    TotalPlayers = g.Games.Sum(ga => ga.Purchases.Count)
+                })
+                .OrderByDescending(g => g.TotalPlayers)
+                .ThenBy(g => g.Id)
+                .ToArray();
+
+
+            return JsonConvert.SerializeObject(gamesByGenres, Formatting.Indented);
         }
 
         public static string ExportUserPurchasesByType(VaporStoreDbContext context, string purchaseType)
         {
-            return "TODO";
+            XmlRootAttribute root = new XmlRootAttribute("Users");
+            XmlSerializerNamespaces namespaces = new XmlSerializerNamespaces();
+            namespaces.Add(string.Empty, string.Empty);
+            XmlSerializer serializer = new XmlSerializer(typeof(UserOutputXmlDto[]), root);
+
+            PurchaseType type = Enum.Parse<PurchaseType>(purchaseType);
+
+            var users = context
+                .Users
+                .Where(u => u.Cards.Any(c => c.Purchases.Any()))
+                .Select(u => new UserOutputXmlDto
+                {
+                    Username = u.Username,
+                    Purchases = context
+                        .Purchases
+                        .Where(p => p.Card.User.Username == u.Username && p.Type == type)
+                        .OrderBy(p => p.Date)
+                        .Select(p => new PurchaseOutputXmlDto
+                        {
+                            Card = p.Card.Number,
+                            Cvc = p.Card.Cvc,
+                            Date = p.Date.ToString("yyyy-MM-dd HH:mm", CultureInfo.InvariantCulture),
+                            Game = new GameOutputXmlDto
+                            {
+                                Title = p.Game.Name,
+                                Genre = p.Game.Genre.ToString(),
+                                Price = p.Game.Price
+                            }
+                        })
+                        .ToArray(),
+                    TotalSpent = context
+                        .Purchases
+                        .ToArray()
+                        .Where(p => p.Card.User.Username == u.Username && p.Type == type)
+                        .Sum(p => p.Game.Price)
+                })
+                .Where(u => u.Purchases.Length > 0)
+                .OrderByDescending(u => u.TotalSpent)
+                .ThenBy(u => u.Username)
+                .ToArray();
+
+            StringBuilder sb = new StringBuilder();
+            using StringWriter sw = new StringWriter(sb);
+            serializer.Serialize(sw, users, namespaces);
+
+            return sb.ToString().TrimEnd();
         }
     }
 }
